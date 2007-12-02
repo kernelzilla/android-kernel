d4c4374e6a0b3e934e520f3d562cc94163c9af34
Arve Hjønnevåg <arve@android.com>
2007-12-01 18:47:49 -0800

    [ARM] msm: gpio support
    Provide an implementation of the generic gpio API for the MSM family.
    
    An extended API provides single-step configuration and access to
    edge detect status.
    
    Signed-off-by: Brian Swetland <swetland@google.com>
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -32,7 +32,6 @@ struct msm_mddi_platform_data
 void __init msm_add_devices(void);
 void __init msm_map_common_io(void);
 void __init msm_init_irq(void);
-void __init msm_init_gpio(void);
 void __init msm_clock_init(void);
 
 #endif
