7185899599855149bb9840f230c4f5cf1b6d8b74
Paul Walmsley <paul@pwsan.com>
2008-10-18 15:59:10 -0600

    OMAP2/3 omapdev: add basic omapdev structure
    Add a structure 'omapdev' to represent on-chip hardware peripherals.
    Currently the only additional information stored are the powerdomains for
    each on-chip peripheral, although the long-term intention is to use this
    to handle device reset at boot time and to clean up device registration.
    
    Use platform_device name and id fields to link this structure back to the
    corresponding platform_device.  In the medium- to long-term, it may be best
    to use a generic device pointer so system devices can be linked also; but
    for now, most cases can be handled via platform_device.
    
    struct omapdev differs in purpose from struct platform_device in that
    platform_device is currently used to represent "logical" devices in
    the Linux driver model, whereas this struct omapdev is used to
    represent physical on-chip devices in the OMAP memory map.  An example
    is the OMAP2xxx camera driver.  It uses only one platform_device,
    "omap24xxcam.-1", but on the hardware, that platform_device covers
    several independent on-chip devices with their own address space and
    control parameters.  Other examples of multiple omapdevs per platform_device
    include the display subsystem and UARTs.
    
    struct omapdev entries are only for on-chip devices.  Off-chip
    peripherals are not included in omapdev.  So, smc91x and onenand, for example,
    will not be included.  What belongs in those two cases would be some sort
    of GPMC platform_device.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

---
+++ arch/arm/mach-omap2/omapdev-common.h
@@ -0,0 +1,27 @@
+/*
+ * OMAP on-chip devices present on OMAP2/3
+ *
+ * Copyright (C) 2007-2008 Texas Instruments, Inc.
+ * Copyright (C) 2007-2008 Nokia Corporation
+ * Paul Walmsley
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#ifndef ARCH_ARM_MACH_OMAP2_OMAPDEV_COMMON_H
+#define ARCH_ARM_MACH_OMAP2_OMAPDEV_COMMON_H
+
+#include <mach/omapdev.h>
+
+#include "omapdev242x.h"
+#include "omapdev243x.h"
+#include "omapdev3xxx.h"
+
+static struct omapdev *omapdevs[] = {
+	NULL,
+};
+
+#endif
