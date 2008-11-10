9bf15c859360a054659ef7524b77a767e56fbb5d
Rajendra Nayak <rnayak@ti.com>
2008-11-10 16:59:10 +0530

    OMAP3 SRF: Adds sysfs control for VDD1/VDD2 OPP's
    Adds sysfs hooks to control VDD1 and VDD2 OPP's
    in the absense of a CPUFreq driver/governor
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -10,12 +10,69 @@
  */
 
 #include <linux/kernel.h>
+#include <linux/device.h>
+
+#include <plat/resource.h>
 
 #include "pm.h"
+
+#ifdef CONFIG_OMAP_PM_SRF
+static ssize_t vdd_opp_show(struct kobject *kobj, struct kobj_attribute *attr,
+			 char *buf)
+{
+	if (attr == &vdd1_opp_attr)
+		return sprintf(buf, "%hu\n", resource_get_level("vdd1_opp"));
+	else if (attr == &vdd2_opp_attr)
+		return sprintf(buf, "%hu\n", resource_get_level("vdd2_opp"));
+	else
+		return -EINVAL;
+}
+
+static ssize_t vdd_opp_store(struct kobject *kobj, struct kobj_attribute *attr,
+			  const char *buf, size_t n)
+{
+	unsigned short value;
+
+	if (sscanf(buf, "%hu", &value) != 1)
+		return -EINVAL;
+
+	if (attr == &vdd1_opp_attr) {
+		if (value < 1 || value > 5) {
+			printk(KERN_ERR "vdd_opp_store: Invalid value\n");
+			return -EINVAL;
+		}
+		resource_request("vdd1_opp", &dummy_sysfs_dev, value);
+	} else if (attr == &vdd2_opp_attr) {
+		if (value < 2 || value > 3) {
+			printk(KERN_ERR "vdd_opp_store: Invalid value\n");
+			return -EINVAL;
+		}
+		resource_request("vdd2_opp", &dummy_sysfs_dev, value);
+	} else {
+		return -EINVAL;
+	}
+	return n;
+}
+#endif
 
 static int __init omap_pm_init(void)
 {
 	int error = -EINVAL;
+
+#ifdef CONFIG_OMAP_PM_SRF
+	error = sysfs_create_file(power_kobj,
+				  &vdd1_opp_attr.attr);
+	if (error) {
+		printk(KERN_ERR "sysfs_create_file failed: %d\n", error);
+		return error;
+	}
+	error = sysfs_create_file(power_kobj,
+				  &vdd2_opp_attr.attr);
+	if (error) {
+		printk(KERN_ERR "sysfs_create_file failed: %d\n", error);
+		return error;
+	}
+#endif
 
 	return error;
 }
