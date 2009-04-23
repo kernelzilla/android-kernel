10d36049699b50c562437184ed02842e0ed15bf9
Dima Zavin <dima@android.com>
2009-04-23 16:11:33 -0700

    [ARM] omap2: sholes: Enable the Quantum touchscreen device
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-omap2/board-sholes.c
+++ arch/arm/mach-omap2/board-sholes.c
@@ -21,6 +21,7 @@
 #include <linux/platform_device.h>
 #include <linux/delay.h>
 #include <linux/input.h>
+#include <linux/interrupt.h>
 #include <linux/err.h>
 #include <linux/clk.h>
 #include <linux/spi/spi.h>
@@ -40,6 +41,8 @@
 #include <mach/board.h>
 #include <mach/common.h>
 #include <mach/gpmc.h>
+
+#include <linux/qtouch_obp_ts.h>
 
 #if 0
 #ifdef CONFIG_VIDEO_OMAP3
@@ -112,7 +115,8 @@ struct dvfs_config omap3_vdd2_config[PRCM_NO_VDD2_OPPS] = {
 #endif
 #define PM_RECEIVER             TWL4030_MODULE_PM_RECEIVER
 
-
+#define SHOLES_TOUCH_RESET_N_GPIO	164
+#define SHOLES_TOUCH_INT_GPIO		99
 
 static struct omap2_mcspi_device_config cpcap_mcspi_config = {
 	.turbo_mode	= 0,
@@ -435,6 +439,115 @@ static struct omap_board_config_kernel sholes_config[] __initdata = {
 	/* {OMAP_TAG_MMC,		&sholes_mmc_config },*/
 };
 
+static int sholes_touch_reset(void)
+{
+	gpio_direction_output(SHOLES_TOUCH_RESET_N_GPIO, 1);
+	msleep(1);
+	gpio_set_value(SHOLES_TOUCH_RESET_N_GPIO, 0);
+	msleep(20);
+	gpio_set_value(SHOLES_TOUCH_RESET_N_GPIO, 1);
+	msleep(20);
+
+	return 0;
+}
+
+static void sholes_touch_init(void)
+{
+	gpio_request(SHOLES_TOUCH_RESET_N_GPIO, "sholes touch reset");
+	gpio_direction_output(SHOLES_TOUCH_RESET_N_GPIO, 1);
+	omap_cfg_reg(H19_34XX_GPIO164_OUT);
+
+	gpio_request(SHOLES_TOUCH_INT_GPIO, "sholes touch irq");
+	gpio_direction_input(SHOLES_TOUCH_INT_GPIO);
+	omap_cfg_reg(AG17_34XX_GPIO99);
+}
+
+static struct qtouch_key sholes_touch_key_list[] = {
+	{
+		.channel	= 0,
+		.code		= KEY_MENU,
+	},
+	{
+		.channel	= 3,
+		.code		= KEY_HOME,
+	},
+	{
+		.channel	= 6,
+		.code		= KEY_BACK,
+	},
+};
+
+static struct qtouch_ts_platform_data sholes_ts_platform_data = {
+	.irqflags	= IRQF_TRIGGER_LOW,
+	.flags		= (QTOUCH_SWAP_XY |
+			   QTOUCH_USE_MULTITOUCH |
+			   QTOUCH_USE_KEYARRAY |
+			   QTOUCH_CFG_BACKUPNV),
+	.abs_min_x	= 0,
+	.abs_max_x	= 1024,
+	.abs_min_y	= 0,
+	.abs_max_y	= 1024,
+	.abs_min_p	= 0,
+	.abs_max_p	= 255,
+	.abs_min_w	= 0,
+	.abs_max_w	= 15,
+	.fuzz_x		= 0,
+	.fuzz_y		= 0,
+	.fuzz_p		= 2,
+	.fuzz_w		= 2,
+	.hw_reset	= sholes_touch_reset,
+	.power_cfg	= {
+		.idle_acq_int	= 1,
+		.active_acq_int	= 16,
+		.active_idle_to	= 25,
+	},
+	.acquire_cfg	= {
+		.charge_time	= 10,
+		.atouch_drift	= 5,
+		.touch_drift	= 20,
+		.drift_susp	= 20,
+		.touch_autocal	= 0,
+		.sync		= 0,
+	},
+	.multi_touch_cfg	= {
+		.x_origin	= 0,
+		.y_origin	= 0,
+		.x_size		= 10,
+		.y_size		= 7,
+		.aks_cfg	= 1,
+		.burst_len	= 0x50,
+		.tch_det_thr	= 0xf,
+		.tch_det_int	= 0x2,
+		.mov_hyst_init	= 5,
+		.mov_hyst_next	= 5,
+		.mov_filter	= 0,
+		.num_touch	= 4,
+		.merge_hyst	= 0,
+		.merge_thresh	= 3,
+	},
+	.key_array		= {
+		.keys		= sholes_touch_key_list,
+		.num_keys	= ARRAY_SIZE(sholes_touch_key_list),
+		.cfg		= {
+			.x_origin	= 11,
+			.y_origin	= 0,
+			.x_size		= 1,
+			.y_size		= 7,
+			.aks_cfg	= 1,
+			.burst_len	= 0x50,
+			.tch_det_thr	= 0xf,
+			.tch_det_int	= 0x2,
+		},
+	},
+};
+
+static struct i2c_board_info __initdata sholes_i2c_bus1_board_info[] = {
+	{
+		I2C_BOARD_INFO(QTOUCH_TS_NAME, 0x11),
+		.platform_data = &sholes_ts_platform_data,
+		.irq = OMAP_GPIO_IRQ(SHOLES_TOUCH_INT_GPIO),
+	},
+};
 
 /*
 static struct i2c_board_info __initdata sholes_i2c_bus3_board_info[] = {
@@ -447,7 +560,8 @@ static struct i2c_board_info __initdata sholes_i2c_bus3_board_info[] = {
 
 static int __init sholes_i2c_init(void)
 {
-	omap_register_i2c_bus(1, 400, NULL, 0);
+	omap_register_i2c_bus(1, 400, sholes_i2c_bus1_board_info,
+			      ARRAY_SIZE(sholes_i2c_bus1_board_info));
 /*
 	omap_register_i2c_bus(2, 100, sholes_i2c_bus2_board_info,
 			      ARRAY_SIZE(sholes_i2c_bus2_board_info));
@@ -473,6 +587,7 @@ static void __init sholes_init(void)
 	omap_serial_init();
 	sholes_panel_init();
 	sholes_sensors_init();
+	sholes_touch_init();
 	//usb_musb_init();
 	//usb_ehci_init();
 	//hsmmc_init();
