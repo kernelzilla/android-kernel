0b4cba82adcc5750a08019832c284b03ea8a1e2b
Greg Meiste <w30289@motorola.com>
2009-05-15 13:36:48 -0700

    [ARM] omap2: sholes: Remove regulators from board file.
    Regulators are now defined and handled in drivers/regulator/cpcap-regulator.c
    
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- arch/arm/mach-omap2/board-sholes-spi.c
+++ arch/arm/mach-omap2/board-sholes-spi.c
@@ -51,33 +51,9 @@ struct cpcap_spi_init_data sholes_cpcap_spi_init[] = {
 	{CPCAP_REG_GPIO6,     0x0000},
 };
 
-struct cpcap_regulator_data sholes_cpcap_regulator_init[] = {
-	{SW5, MODE_ON, MODE_ON, MODE_ON},
-	{VAUDIO, MODE_ON, MODE_ON, MODE_ON},
-	{VCAM, MODE_ON, MODE_ON, MODE_OFF},
-	{VCSI, MODE_ON, MODE_OFF, MODE_ON},
-	{VDAC, MODE_ON, MODE_ON, MODE_OFF},
-	{VDIG, MODE_ON, MODE_ON, MODE_OFF},
-	{VFUSE, MODE_ON, MODE_ON, MODE_OFF},
-	{VHVIO, MODE_ON, MODE_ON, MODE_OFF},
-	{VSDIO, MODE_ON, MODE_ON, MODE_OFF},
-	{VPLL, MODE_ON, MODE_OFF, MODE_ON},
-	{VRF1, MODE_ON, MODE_OFF, MODE_OFF},
-	{VRF2, MODE_ON, MODE_OFF, MODE_OFF},
-	{VRFREF, MODE_ON, MODE_OFF, MODE_ON},
-	{VWLAN1, MODE_ON, MODE_ON, MODE_OFF},
-	{VWLAN2, MODE_ON, MODE_OFF, MODE_OFF},
-	{VSIM, MODE_ON, MODE_LOW_POWER, MODE_ON},
-	{VSIMCARD, MODE_ON, MODE_LOW_POWER, MODE_ON},
-	{VUSB, MODE_ON, MODE_OFF, MODE_ON},
-	{VVIB, MODE_ON, MODE_ON, MODE_OFF}
-};
-
 static struct cpcap_platform_data sholes_cpcap_data = {
 	.init = sholes_cpcap_spi_init,
 	.init_len = ARRAY_SIZE(sholes_cpcap_spi_init),
-	.regulator_init = sholes_cpcap_regulator_init,
-	.regulator_init_len = ARRAY_SIZE(sholes_cpcap_spi_init),
 };
 
 static struct omap2_mcspi_device_config tsc2005_mcspi_config = {
