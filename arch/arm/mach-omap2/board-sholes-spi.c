9c8ee140bb9f8e7fa011b2173c683640e212b206
Rebecca Schultz Zavin <rebecca@android.com>
2009-06-22 15:41:06 -0700

    [ARM] omap2: sholes: Updating regulator and cpcap initialization for sholes
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- arch/arm/mach-omap2/board-sholes-spi.c
+++ arch/arm/mach-omap2/board-sholes-spi.c
@@ -32,25 +32,22 @@ struct cpcap_spi_init_data sholes_cpcap_spi_init[] = {
 	{CPCAP_REG_ASSIGN4,   0x0000},
 	{CPCAP_REG_ASSIGN5,   0x0000},
 	{CPCAP_REG_ASSIGN6,   0x0000},
-	{CPCAP_REG_MIM2,      0x0000},
+	{CPCAP_REG_UCC1,      0x0000},
 	{CPCAP_REG_PC1,       0x010A},
 	{CPCAP_REG_PC2,       0x0150},
 	{CPCAP_REG_PGC,       0x0000},
 	{CPCAP_REG_SDVSPLL,   0xDB04},
 	{CPCAP_REG_SI2CC1,    0x0201},
 	{CPCAP_REG_Si2CC2,    0x00C4},
-	{CPCAP_REG_S1C1,      0x6434},
-	{CPCAP_REG_S1C2,      0x3C14},
+	{CPCAP_REG_S1C1,      0x6438},
+	{CPCAP_REG_S1C2,      0x3838},
 	{CPCAP_REG_S2C1,      0x6434},
 	{CPCAP_REG_S2C2,      0x3C14},
-	{CPCAP_REG_S3C,       0x0521},
-	{CPCAP_REG_S4C1,      0x4434},
+	{CPCAP_REG_S3C,       0x0539},
+	{CPCAP_REG_S4C1,      0x0000},
 	{CPCAP_REG_S4C2,      0x3434},
 	{CPCAP_REG_S6C,       0x0000},
-	{CPCAP_REG_VSDIOC,    0x0023},
-	{CPCAP_REG_VRF1C,     0x002C},
-	{CPCAP_REG_VRF2C,     0x000B},
-	{CPCAP_REG_VRFREFC,   0x000B},
+	{CPCAP_REG_VWLAN2C,   0x0001},
 	{CPCAP_REG_VVIBC,     0x000D},   /* Temporary */
 	{CPCAP_REG_VUSBINT1C, 0x0029},
 	{CPCAP_REG_VUSBINT2C, 0x0029},
@@ -62,14 +59,37 @@ struct cpcap_spi_init_data sholes_cpcap_spi_init[] = {
 	{CPCAP_REG_UIER2,     0x001F},
 	{CPCAP_REG_UIEF2,     0x001F},
 	{CPCAP_REG_OWDC,      0x0003},
-	{CPCAP_REG_GPIO0,     0x0000},
-	{CPCAP_REG_GPIO1,     0x0000},
-	{CPCAP_REG_GPIO2,     0x0000},
-	{CPCAP_REG_GPIO3,     0x0000},
-	{CPCAP_REG_GPIO4,     0x0000},
-	{CPCAP_REG_GPIO5,     0x0000},
-	{CPCAP_REG_GPIO6,     0x0000},
-	{CPCAP_REG_VWLAN2C,   0x004D},
+	{CPCAP_REG_GPIO0,     0x3004},
+	{CPCAP_REG_GPIO1,     0x3008},
+	{CPCAP_REG_GPIO2,     0x3204},
+	{CPCAP_REG_GPIO3,     0x3008},
+	{CPCAP_REG_GPIO4,     0x3204},
+	{CPCAP_REG_GPIO5,     0x3008},
+	{CPCAP_REG_GPIO6,     0x3004},
+	{CPCAP_REG_MDLC,      0x0000},
+	{CPCAP_REG_KLC,       0x0000},
+};
+
+unsigned short cpcap_regulator_mode_values[CPCAP_NUM_REGULATORS] = {
+	[CPCAP_SW5]      = 0x0002,
+	[CPCAP_VCAM]     = 0x0003,
+	[CPCAP_VCSI]     = 0x0003,
+	[CPCAP_VDAC]     = 0x0003,
+	[CPCAP_VDIG]     = 0x0003,
+	[CPCAP_VFUSE]    = 0x0080,
+	[CPCAP_VHVIO]    = 0x0003,
+	[CPCAP_VSDIO]    = 0x0003,
+	[CPCAP_VPLL]     = 0x0042,
+	[CPCAP_VRF1]     = 0x000C,
+	[CPCAP_VRF2]     = 0x0003,
+	[CPCAP_VRFREF]   = 0x0003,
+	[CPCAP_VWLAN1]   = 0x0003,
+	[CPCAP_VWLAN2]   = 0x000C,
+	[CPCAP_VSIM]     = 0x0003,
+	[CPCAP_VSIMCARD] = 0x1E00,
+	[CPCAP_VVIB]     = 0x0001,
+	[CPCAP_VUSB]     = 0x000C,
+	[CPCAP_VAUDIO]   = 0x0006,
 };
 
 #define CPCAP_GPIO 0
@@ -118,43 +138,44 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 			.min_uV			= 5050000,
 			.max_uV			= 5050000,
 			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+			.apply_uV		= 1,
 		},
 		.num_consumer_supplies	= ARRAY_SIZE(cpcap_sw5_consumers),
 		.consumer_supplies	= cpcap_sw5_consumers,
 	},
 	[CPCAP_VCAM] = {
 		.constraints = {
-			.min_uV			= 2600000,
-			.max_uV			= 2900000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.min_uV			= 2800000,
+			.max_uV			= 2800000,
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+			.apply_uV		= 1,
+
 		},
 		.num_consumer_supplies	= ARRAY_SIZE(cpcap_vcam_consumers),
 		.consumer_supplies	= cpcap_vcam_consumers,
 	},
 	[CPCAP_VCSI] = {
 		.constraints = {
-			.min_uV			= 1200000,
+			.min_uV			= 1800000,
 			.max_uV			= 1800000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 			.boot_on		= 1,
+			.apply_uV		= 1,
 		},
 	},
 	[CPCAP_VDAC] = {
 		.constraints = {
-			.min_uV			= 1200000,
-			.max_uV			= 2500000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.min_uV			= 1800000,
+			.max_uV			= 1800000,
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+			.apply_uV		= 1,
 		},
 	},
 	[CPCAP_VDIG] = {
 		.constraints = {
 			.min_uV			= 1200000,
 			.max_uV			= 1875000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VFUSE] = {
@@ -169,19 +190,19 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 		.constraints = {
 			.min_uV			= 2775000,
 			.max_uV			= 2775000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 			.boot_on		= 1,
+			.apply_uV		= 1,
 		},
 		.num_consumer_supplies	= ARRAY_SIZE(cpcap_vhvio_consumers),
 		.consumer_supplies	= cpcap_vhvio_consumers,
 	},
 	[CPCAP_VSDIO] = {
 		.constraints = {
-			.min_uV			= 1500000,
-			.max_uV			= 3000000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.min_uV			= 2900000,
+			.max_uV			= 2900000,
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+			.apply_uV		= 1,
 		},
 		.num_consumer_supplies	= ARRAY_SIZE(cpcap_vsdio_consumers),
 		.consumer_supplies	= cpcap_vsdio_consumers,
@@ -190,7 +211,7 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 		.constraints = {
 			.min_uV			= 1800000,
 			.max_uV			= 1800000,
-			.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE,
+			.valid_ops_mask		= 0,
 			.always_on		= 1,
 			.apply_uV		= 1,
 		},
@@ -199,43 +220,36 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 		.constraints = {
 			.min_uV			= 2500000,
 			.max_uV			= 2775000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
-			.boot_on		= 1,
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VRF2] = {
 		.constraints = {
 			.min_uV			= 2775000,
 			.max_uV			= 2775000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
-			.boot_on		= 1,
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VRFREF] = {
 		.constraints = {
 			.min_uV			= 2500000,
 			.max_uV			= 2775000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
-			.boot_on		= 1,
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VWLAN1] = {
 		.constraints = {
 			.min_uV			= 1800000,
 			.max_uV			= 1900000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VWLAN2] = {
 		.constraints = {
-			.min_uV			= 2775000,
-			.max_uV			= 3300000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.min_uV			= 3000000,
+			.max_uV			= 3000000,
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+			.apply_uV		= 1,
 		},
 		.num_consumer_supplies	= ARRAY_SIZE(cpcap_vwlan2_consumers),
 		.consumer_supplies	= cpcap_vwlan2_consumers,
@@ -244,16 +258,14 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 		.constraints = {
 			.min_uV			= 1800000,
 			.max_uV			= 2900000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VSIMCARD] = {
 		.constraints = {
 			.min_uV			= 1800000,
 			.max_uV			= 2900000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= 0,
 		},
 	},
 	[CPCAP_VVIB] = {
@@ -271,15 +283,15 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 		.constraints = {
 			.min_uV			= 3300000,
 			.max_uV			= 3300000,
-			.valid_ops_mask		= (REGULATOR_CHANGE_VOLTAGE |
-						   REGULATOR_CHANGE_STATUS),
+			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+			.apply_uV		= 1,
 		},
 	},
 	[CPCAP_VAUDIO] = {
 		.constraints = {
 			.min_uV			= 2775000,
 			.max_uV			= 2775000,
-			.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE,
+			.valid_ops_mask		= 0,
 			.always_on		= 1,
 			.apply_uV		= 1,
 		},
@@ -302,6 +314,7 @@ static struct cpcap_adc_ato sholes_cpcap_adc_ato = {
 static struct cpcap_platform_data sholes_cpcap_data = {
 	.init = sholes_cpcap_spi_init,
 	.init_len = ARRAY_SIZE(sholes_cpcap_spi_init),
+	.regulator_mode_values = cpcap_regulator_mode_values,
 	.regulator_init = cpcap_regulator,
 	.adc_ato = &sholes_cpcap_adc_ato,
 };
