d60788e2b79568139a200c3356d52b369d8d24b3
J. R. Okajima <hooanon05@yahoo.co.jp>
2009-07-14 13:33:20 +0900

    aufs: bugfix, protect RR mode in MFS mode
    au_wbr_create_mfsrr() implements 'create=mfsrr' mount option which tries
    Most-Free-Space policy first, and then Round-Robin policy if necessary.
    Before au_wbr_create_mfsrr() goes into RR mode, acquire mfs->mfs_lock.
    
    Signed-off-by: J. R. Okajima <hooanon05@yahoo.co.jp>

--- fs/aufs/wbr_policy.c
+++ fs/aufs/wbr_policy.c
@@ -460,8 +460,10 @@ static int au_wbr_create_mfsrr(struct dentry *dentry, int isdir)
 	err = au_wbr_create_mfs(dentry, isdir);
 	if (err >= 0) {
 		mfs = &au_sbi(dentry->d_sb)->si_wbr_mfs;
+		mutex_lock(&mfs->mfs_lock);
 		if (mfs->mfsrr_bytes < mfs->mfsrr_watermark)
 			err = au_wbr_create_rr(dentry, isdir);
+		mutex_unlock(&mfs->mfs_lock);
 	}
 
 	AuDbg("b%d\n", err);
