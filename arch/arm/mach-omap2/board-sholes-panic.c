ce907aee6087927fe5da0f8a88c181a67a4b55aa
San Mehat <san@google.com>
2009-08-20 11:01:12 -0700

    [ARM] mach-omap2: sholes-panic: Revamp the kpanic driver
    - Buffer is no longer required to hold the flash data
    - new separated /proc/last_console /proc/last_threads files
    - compatibility /proc/kmsg still there
    - Data is now erased when writing to the /proc files
    - I/O is now performed in native flash page sizes (Thanks Dong-Dong Yang)
    - Now choose panic_write() vs write() based on in_interrupt()
    - Cleanups
    
    Signed-off-by: San Mehat <san@google.com>

--- arch/arm/mach-omap2/board-sholes-panic.c
+++ arch/arm/mach-omap2/board-sholes-panic.c
@@ -1,4 +1,4 @@
-/* arch/arm/mach-msm/smd_rpcrouter.c
+/* arch/arm/mach-omap2/board-sholes-panic.c
  *
  * Copyright (C) 2007 Google, Inc.
  * Author: San Mehat <san@android.com>
@@ -34,11 +34,12 @@
 #include <linux/debugfs.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
-#include <linux/spinlock.h>
+#include <linux/mutex.h>
+#include <linux/workqueue.h>
 
 #define PANIC_PARTITION "kpanic"
 
-#define EXPERIMENTAL_BB_SKIP 0
+#define EXPERIMENTAL_BB_SKIP 1
 
 struct panic_header {
 	u32 magic;
@@ -49,15 +50,20 @@ struct panic_header {
 
 	u32 threads_offset;
 	u32 threads_length;
-
 };
 
-static struct mtd_info *panic_mtd_dev = NULL;
-static void *mtd_bounce_page = NULL;
+struct sholes_panic_data {
+	struct mtd_info		*mtd;
+	struct panic_header	curr;
+	void			*bounce;
+	struct proc_dir_entry	*last_console;
+	struct proc_dir_entry	*last_threads;
+	struct proc_dir_entry	*last_kmsg;
+};
 
-static DEFINE_SPINLOCK(kmsg_lock);
-static void *kmsg_data = NULL;
-static unsigned kmsg_data_len = 0;
+static struct sholes_panic_data drv_ctx;
+static struct work_struct proc_removal_work;
+static DEFINE_MUTEX(drv_mutex);
 
 static void sholes_panic_erase_callback(struct erase_info *done)
 {
@@ -65,152 +71,91 @@ static void sholes_panic_erase_callback(struct erase_info *done)
 	wake_up(wait_q);
 }
 
-static ssize_t sholes_panic_proc_read(struct file *file, char __user *buf,
-				     size_t len, loff_t *offset)
+static int sholes_panic_proc_read(char *buffer, char **start, off_t offset,
+			       int count, int *peof, void *dat)
 {
-	unsigned long flags;
-	loff_t pos = *offset;
-	ssize_t count;
+	struct sholes_panic_data *ctx = &drv_ctx;
+	size_t file_length;
+	off_t file_offset;
+	unsigned int page_no;
+	off_t page_offset;
+	int rc;
+	size_t len;
 
-	spin_lock_irqsave(&kmsg_lock, flags);
-	if (pos >= kmsg_data_len)
+	if (!count)
 		return 0;
 
-	count = min(len, (size_t)(kmsg_data_len - pos));
-	if (copy_to_user(buf, kmsg_data + pos, count)) {
-		spin_unlock_irqrestore(&kmsg_lock, flags);
-		return -EFAULT;
+	mutex_lock(&drv_mutex);
+	
+	switch ((int) dat) {
+	case 1:	/* last_console */
+		file_length = ctx->curr.console_length;
+		file_offset = ctx->curr.console_offset;
+		break;
+	case 2:	/* last_threads */
+		file_length = ctx->curr.threads_length;
+		file_offset = ctx->curr.threads_offset;
+		break;
+	case 3:	/* last_kmsg */
+		file_length = ctx->curr.threads_length + ctx->curr.console_length;
+		file_offset = ctx->curr.console_offset;
+		break;
+	default:
+		pr_err("Bad dat (%d)\n", (int) dat);
+		mutex_unlock(&drv_mutex);
+		return -EINVAL;
 	}
-	*offset += count;
-	spin_unlock_irqrestore(&kmsg_lock, flags);
+
+	if ((offset + count) > file_length) {
+		mutex_unlock(&drv_mutex);
+		return 0;
+	}
+
+	/* We only support reading a maximum of a flash page */
+	if (count > ctx->mtd->writesize)
+		count = ctx->mtd->writesize;
+
+	page_no = (file_offset + offset) / ctx->mtd->writesize;
+	page_offset = (file_offset + offset) % ctx->mtd->writesize;
+
+	rc = ctx->mtd->read(ctx->mtd,
+			    (page_no * ctx->mtd->writesize),
+			    ctx->mtd->writesize,
+			    &len, ctx->bounce);
+
+	if (page_offset)
+		count -= page_offset;
+	memcpy(buffer, ctx->bounce + page_offset, count);
+
+	*start = count;
+
+	if ((offset + count) == file_length)
+		*peof = 1;
+	
+	mutex_unlock(&drv_mutex);
 	return count;
 }
 
-static ssize_t sholes_panic_proc_write(struct file *file,
-				       const char __user *buf,
-				       size_t len, loff_t *offset)
+static void mtd_panic_erase(void)
 {
-	unsigned long flags;
-	spin_lock_irqsave(&kmsg_lock, flags);
-	if (kmsg_data) {
-		kfree(kmsg_data);
-		kmsg_data = NULL;
-		kmsg_data_len = 0;
-		printk(KERN_INFO "sholes-panic: kmsg buffer freed\n");
-	}
-	spin_unlock_irqrestore(&kmsg_lock, flags);
-	return len;
-}
-
-static const struct file_operations sholes_panic_file_ops = {
-	.owner = THIS_MODULE,
-	.read = sholes_panic_proc_read,
-	.write = sholes_panic_proc_write,
-};
-
-static void mtd_panic_notify_add(struct mtd_info *mtd)
-{
-	struct proc_dir_entry *entry;
-	struct panic_header *hdr = mtd_bounce_page;
-	size_t len;
+	struct sholes_panic_data *ctx = &drv_ctx;
 	struct erase_info erase;
 	DECLARE_WAITQUEUE(wait, current);
 	wait_queue_head_t wait_q;
 	int rc, i;
 
-	if (strcmp(mtd->name, PANIC_PARTITION))
-		return;
-
-	panic_mtd_dev = mtd;
-
-	/* If theres a dump currently in flash, suck it out */
-	if (!mtd->read) {
-		printk(KERN_ERR "sholes_panic: No read available on mtd\n");
-		return;
-	}
-
-	if (mtd->block_isbad(mtd, 0)) {
-		printk(KERN_ERR "sholes_panic: Offset 0 bad block. Boourns!\n");
-		panic_mtd_dev = NULL;
-		return;
-	}
-
-	rc = mtd->read(mtd, 0, PAGE_SIZE, &len, mtd_bounce_page);
-	if (rc) {
-		printk(KERN_ERR "sholes_panic: Err reading flash (%d)\n", rc);
-		return;
-	}
-	if (len != PAGE_SIZE) {
-		printk(KERN_ERR "sholes_panic: Bad read size (%d)\n", rc);
-		return;
-	}
-
-	printk(KERN_INFO "Sholes panic driver bound to %s\n", mtd->name);
-
-	if (hdr->magic != PANIC_MAGIC) {
-		printk(KERN_INFO "sholes_panic: No panic data available\n");
-		goto erase;
-	}
-
-	printk(KERN_INFO "sholes_panic: c(%u, %u) t(%u, %u)\n",
-	       hdr->console_offset, hdr->console_length,
-	       hdr->threads_offset, hdr->threads_length);
-
-	entry = create_proc_entry("last_kmsg", S_IFREG | S_IRUGO, NULL);
-	if (!entry) {
-		printk(KERN_ERR "sholes_panic: failed to create proc entry\n");
-		return;
-	}
-	entry->proc_fops = &sholes_panic_file_ops;
-	entry->size = hdr->console_length + hdr->threads_length;
-
-	kmsg_data = kmalloc(ALIGN(entry->size, PAGE_SIZE), GFP_KERNEL);
-	if (!kmsg_data) {
-		printk(KERN_ERR "sholes_panic: failed to alloc buffer\n");
-		remove_proc_entry("last_kmsg", NULL);
-		return;
-	}
-	memset(kmsg_data, 0, ALIGN(entry->size, PAGE_SIZE));
-	printk("sholes_panic: Allocated %llu bytes for kmsg buffer\n",
-	       (ALIGN(entry->size, PAGE_SIZE)));
-
-	for (i = 0; i < hdr->console_length; i += PAGE_SIZE) {
-		rc = mtd->read(mtd,
-			       i + hdr->console_offset,
-			       PAGE_SIZE, &len, kmsg_data + i);
-		if (rc) {
-			printk(KERN_ERR "sholes_panic: Err reading console\n");
-			remove_proc_entry("last_kmsg", NULL);
-			return;
-		}
-	}
-	kmsg_data_len = hdr->console_length;
-
-	for (i = 0; i < hdr->threads_length; i += PAGE_SIZE) {
-		rc = mtd->read(mtd,
-			       i + hdr->threads_offset,
-			       PAGE_SIZE, &len, kmsg_data + kmsg_data_len + i);
-		if (rc) {
-			printk(KERN_ERR "sholes_panic: Err reading threads\n");
-			remove_proc_entry("last_kmsg", NULL);
-			return;
-		}
-	}
-	kmsg_data_len += hdr->threads_length;
-
-erase:
-
+	printk("sholes_panic: Erasing crash partition\n");
 	init_waitqueue_head(&wait_q);
-	erase.mtd = mtd;
+	erase.mtd = ctx->mtd;
 	erase.callback = sholes_panic_erase_callback;
-	erase.len = mtd->erasesize;
+	erase.len = ctx->mtd->erasesize;
 	erase.priv = (u_long)&wait_q;
-	for (i = 0; i < mtd->size; i += mtd->erasesize) {
+	for (i = 0; i < ctx->mtd->size; i += ctx->mtd->erasesize) {
 		erase.addr = i;
 		set_current_state(TASK_INTERRUPTIBLE);
 		add_wait_queue(&wait_q, &wait);
 
-		rc = mtd->block_isbad(mtd, erase.addr);
+		rc = ctx->mtd->block_isbad(ctx->mtd, erase.addr);
 		if (rc < 0) {
 			printk(KERN_ERR
 			       "sholes_panic: Bad block check "
@@ -226,7 +171,7 @@ erase:
 			continue;
 		}
 
-		rc = mtd->erase(mtd, &erase);
+		rc = ctx->mtd->erase(ctx->mtd, &erase);
 		if (rc) {
 			set_current_state(TASK_RUNNING);
 			remove_wait_queue(&wait_q, &wait);
@@ -234,21 +179,155 @@ erase:
 			       "sholes_panic: Erase of region 0x%llx, 0x%llx failed\n",
 			       (unsigned long long) erase.addr,
 			       (unsigned long long) erase.len);
+			if (rc == -EIO) {
+				if (ctx->mtd->block_markbad(ctx->mtd, erase.addr)) {
+					printk(KERN_ERR
+					       "sholes_panic: Err marking "
+					       "block bad\n");
+					goto out;
+				}
+				printk(KERN_INFO
+				       "sholes_panic: Marked a bad block"
+				       " @%llx\n", erase.addr);
+				continue;
+			}
 			goto out;
 		}
 		schedule();
 		remove_wait_queue(&wait_q, &wait);
 	}
-	printk(KERN_DEBUG "sholes_panic: Panic partition cleared\n");
+	printk(KERN_DEBUG "sholes_panic: Panic partition erased\n");
 out:
 	return;
 }
 
+static void sholes_panic_remove_proc_work(struct work_struct *work)
+{
+	struct sholes_panic_data *ctx = &drv_ctx;
+
+	mutex_lock(&drv_mutex);
+	mtd_panic_erase();
+	memset(&ctx->curr, 0, sizeof(struct panic_header));
+	if (ctx->last_console) {
+		remove_proc_entry("last_console", NULL);
+		ctx->last_console = NULL;
+	}
+	if (ctx->last_threads) {
+		remove_proc_entry("last_threads", NULL);
+		ctx->last_threads = NULL;
+	}
+	if (ctx->last_kmsg) {
+		remove_proc_entry("last_kmsg", NULL);
+		ctx->last_kmsg = NULL;
+	}
+	mutex_unlock(&drv_mutex);
+}
+
+static int sholes_panic_proc_write(struct file *file, const char __user *buffer,
+				unsigned long count, void *data)
+{
+	schedule_work(&proc_removal_work);
+	return count;
+}
+
+static void mtd_panic_notify_add(struct mtd_info *mtd)
+{
+	struct sholes_panic_data *ctx = &drv_ctx;
+	struct panic_header *hdr = ctx->bounce;
+	size_t len;
+	int rc;
+
+	if (strcmp(mtd->name, PANIC_PARTITION))
+		return;
+
+	ctx->mtd = mtd;
+
+	if (mtd->block_isbad(mtd, 0)) {
+		printk(KERN_ERR "sholes_panic: Offset 0 bad block. Boourns!\n");
+		goto out_err;
+	}
+
+	rc = mtd->read(mtd, 0, mtd->writesize, &len, ctx->bounce);
+	if (rc && rc == -EBADMSG) {
+		printk(KERN_WARNING
+		       "sholes_panic: Bad ECC on block 0 (ignored)\n");
+	} else if (rc && rc != -EUCLEAN) {
+		printk(KERN_ERR "sholes_panic: Error reading block 0 (%d)\n", rc);
+		goto out_err;
+	}
+
+	if (len != mtd->writesize) {
+		printk(KERN_ERR "sholes_panic: Bad read size (%d)\n", rc);
+		goto out_err;
+	}
+
+	printk(KERN_INFO "Sholes panic driver bound to %s\n", mtd->name);
+
+	if (hdr->magic != PANIC_MAGIC) {
+		printk(KERN_INFO "sholes_panic: No panic data available\n");
+		return;
+	}
+
+	memcpy(&ctx->curr, hdr, sizeof(struct panic_header));
+
+	printk(KERN_INFO "sholes_panic: c(%u, %u) t(%u, %u)\n",
+	       hdr->console_offset, hdr->console_length,
+	       hdr->threads_offset, hdr->threads_length);
+
+	if (hdr->console_length) {
+		ctx->last_console = create_proc_entry("last_console",
+						      S_IFREG | S_IRUGO, NULL);
+		if (!ctx->last_console)
+			printk(KERN_ERR "%s: failed creating procfile\n",
+			       __func__);
+		else {
+			ctx->last_console->read_proc = sholes_panic_proc_read;
+			ctx->last_console->write_proc = sholes_panic_proc_write;
+			ctx->last_console->size = hdr->console_length;
+			ctx->last_console->data = (void *) 1;
+		}
+	}
+
+	if (hdr->threads_length) {
+		ctx->last_threads = create_proc_entry("last_threads",
+						       S_IFREG | S_IRUGO, NULL);
+		if (!ctx->last_threads)
+			printk(KERN_ERR "%s: failed creating procfile\n",
+			       __func__);
+		else {
+			ctx->last_threads->read_proc = sholes_panic_proc_read;
+			ctx->last_threads->write_proc = sholes_panic_proc_write;
+			ctx->last_threads->size = hdr->threads_length;
+			ctx->last_threads->data = (void *) 2;
+		}
+	}
+
+	if (hdr->console_length || hdr->threads_length) {
+		ctx->last_kmsg = create_proc_entry("last_kmsg",
+						   S_IFREG | S_IRUGO, NULL);
+		if (!ctx->last_kmsg)
+			printk(KERN_ERR "%s: failed creating procfile\n",
+			       __func__);
+		else {
+			ctx->last_kmsg->read_proc = sholes_panic_proc_read;
+			ctx->last_kmsg->write_proc = sholes_panic_proc_write;
+			ctx->last_kmsg->size = hdr->console_length +
+					       hdr->threads_length;
+			ctx->last_kmsg->data = (void *) 3;
+		}
+	}
+	return;
+out_err:
+	ctx->mtd = NULL;
+}
+
 static void mtd_panic_notify_remove(struct mtd_info *mtd)
 {
-	if (mtd == panic_mtd_dev)
-		panic_mtd_dev = NULL;
-	printk(KERN_INFO "Sholes panic driver unbound from %s\n", mtd->name);
+	struct sholes_panic_data *ctx = &drv_ctx;
+	if (mtd == ctx->mtd) {
+		ctx->mtd = NULL;
+		printk(KERN_INFO "Sholes panic driver unbound from %s\n", mtd->name);
+	}
 }
 
 static struct mtd_notifier mtd_panic_notifier = {
@@ -258,13 +337,11 @@ static struct mtd_notifier mtd_panic_notifier = {
 
 static int in_panic = 0;
 
-static int sholes_writeflash(struct mtd_info *mtd, loff_t to, size_t num_pages,
-			     const u_char *buf, int panic)
+static int sholes_writeflashpage(struct mtd_info *mtd, loff_t to, const u_char *buf)
 {
 	int rc;
 	size_t wlen;
-
-	panic = 0; /* omap2 write is safe */
+	int panic = in_interrupt();
 
 	if (panic && !mtd->panic_write) {
 		printk(KERN_EMERG "%s: No panic_write available\n", __func__);
@@ -275,9 +352,9 @@ static int sholes_writeflash(struct mtd_info *mtd, loff_t to, size_t num_pages,
 	}
 
 	if (panic)
-		rc = mtd->panic_write(mtd, to, PAGE_SIZE, &wlen, buf);
+		rc = mtd->panic_write(mtd, to, mtd->writesize, &wlen, buf);
 	else
-		rc = mtd->write(mtd, to, PAGE_SIZE, &wlen, buf);
+		rc = mtd->write(mtd, to, mtd->writesize, &wlen, buf);
 
 	if (rc) {
 		printk(KERN_EMERG
@@ -296,21 +373,21 @@ extern void log_buf_clear(void);
  * Writes the contents of the console to the specified offset in flash.
  * Returns number of bytes written
  */
-static int sholes_panic_write_console(struct mtd_info *mtd, unsigned int off,
-				      int panic)
+static int sholes_panic_write_console(struct mtd_info *mtd, unsigned int off)
 {
+	struct sholes_panic_data *ctx = &drv_ctx;
 	int saved_oip;
 	int idx = 0;
 	int rc, rc2;
 	for (;;) {
 		saved_oip = oops_in_progress;
 		oops_in_progress = 1;
-		rc = log_buf_copy(mtd_bounce_page, idx, PAGE_SIZE);
+		rc = log_buf_copy(ctx->bounce, idx, mtd->writesize);
 		oops_in_progress = saved_oip;
 		if (rc <= 0)
 			break;
-		if (rc != PAGE_SIZE)
-			memset(mtd_bounce_page + rc, 0, PAGE_SIZE - rc);
+		if (rc != mtd->writesize)
+			memset(ctx->bounce + rc, 0, mtd->writesize - rc);
 #if EXPERIMENTAL_BB_SKIP
 check_badblock:
 		rc = mtd->block_isbad(mtd, off);
@@ -322,7 +399,7 @@ check_badblock:
 		if (rc) {
 			printk(KERN_WARNING
 			       "sholes_panic: Skipping over bad "
-			       "block @%llx\n", off);
+			       "block @%x\n", off);
 			off += mtd->erasesize;
 			printk("chk %u %llu\n", off, mtd->size);
 			if (off >= mtd->size) {
@@ -335,14 +412,14 @@ check_badblock:
 		}
 #endif /* EXPERIMENTAL_BB_SKIP */
 
-		rc2 = sholes_writeflash(mtd, off, 1, mtd_bounce_page, panic);
+		rc2 = sholes_writeflashpage(mtd, off, ctx->bounce);
 		if (rc2 <= 0) {
 			printk(KERN_EMERG
 			       "sholes_panic: Flash write failed (%d)\n", rc2);
 			return rc2;
 		}
-		idx += rc;
-		off += rc;
+		idx += rc2;
+		off += rc2;
 	}
 	return idx;
 }
@@ -350,26 +427,27 @@ check_badblock:
 static int sholes_panic(struct notifier_block *this, unsigned long event,
 			void *ptr)
 {
-	struct panic_header *hdr = (struct panic_header *) mtd_bounce_page;
-	int sys_panic = in_interrupt();
-	int console_len;
-	int threads_offset;
-	int threads_len;
+	struct sholes_panic_data *ctx = &drv_ctx;
+	struct panic_header *hdr = (struct panic_header *) ctx->bounce;
+	int console_offset = 0;
+	int console_len = 0;
+	int threads_offset = 0;
+	int threads_len = 0;
 	int rc;
 
 	if (in_panic)
 		return NOTIFY_DONE;
 	in_panic = 1;
 
-	if (!panic_mtd_dev)
+	if (!ctx->mtd)
 		goto out;
 
+	console_offset = ctx->mtd->writesize;
 
 	/*
 	 * Write out the console
 	 */
-	console_len = sholes_panic_write_console(panic_mtd_dev, PAGE_SIZE,
-						 sys_panic);
+	console_len = sholes_panic_write_console(ctx->mtd, console_offset);
 	if (console_len < 0) {
 		printk(KERN_EMERG "Error writing console to panic log! (%d)\n",
 		       console_len);
@@ -379,11 +457,11 @@ static int sholes_panic(struct notifier_block *this, unsigned long event,
 	/*
 	 * Write out all threads
 	 */
-	threads_offset = ALIGN(PAGE_SIZE + console_len, PAGE_SIZE);
+	threads_offset = ALIGN(console_offset + console_len,
+			       ctx->mtd->writesize);
 	log_buf_clear();
 	show_state_filter(0);
-	threads_len = sholes_panic_write_console(panic_mtd_dev, threads_offset,
-						 sys_panic);
+	threads_len = sholes_panic_write_console(ctx->mtd, threads_offset);
 	if (threads_len < 0) {
 		printk(KERN_EMERG "Error writing threads to panic log! (%d)\n",
 		       threads_len);
@@ -393,16 +471,16 @@ static int sholes_panic(struct notifier_block *this, unsigned long event,
 	/*
 	 * Finally write the panic header
 	 */
-	memset(mtd_bounce_page, 0, PAGE_SIZE);
+	memset(ctx->bounce, 0, PAGE_SIZE);
 	hdr->magic = PANIC_MAGIC;
 
-	hdr->console_offset = PAGE_SIZE;
+	hdr->console_offset = console_offset;
 	hdr->console_length = console_len;
 
 	hdr->threads_offset = threads_offset;
 	hdr->threads_length = threads_len;
 
-	rc = sholes_writeflash(panic_mtd_dev, 0, 1, mtd_bounce_page, sys_panic);
+	rc = sholes_writeflashpage(ctx->mtd, 0, ctx->bounce);
 	if (rc <= 0) {
 		printk(KERN_EMERG "sholes_panic: Header write failed (%d)\n",
 		       rc);
@@ -439,6 +517,8 @@ void __init sholes_panic_init(void)
 	register_mtd_user(&mtd_panic_notifier);
 	atomic_notifier_chain_register(&panic_notifier_list, &panic_blk);
 	debugfs_create_file("panic", 0644, NULL, NULL, &panic_dbg_fops);
-	mtd_bounce_page = __get_free_page(GFP_KERNEL);
+	memset(&drv_ctx, 0, sizeof(drv_ctx));
+	drv_ctx.bounce = (void *) __get_free_page(GFP_KERNEL);
+        INIT_WORK(&proc_removal_work, sholes_panic_remove_proc_work);
 }
 
