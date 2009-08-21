ed5c2631baf48ff27eecb5d055a3f423d696185f
J. R. Okajima <hooanon05@yahoo.co.jp>
2009-08-21 16:21:51 +0900

    aufs: extract the body of aufs_ioctl_dir()
    Since aufs_ioctl_dir() will grow in the future, extract the current
    contents of aufs_ioctl_dir() and create a new function au_plink_ioctl().
    
    Signed-off-by: J. R. Okajima <hooanon05@yahoo.co.jp>

--- fs/aufs/ioctl.c
+++ fs/aufs/ioctl.c
@@ -28,40 +28,15 @@ long aufs_ioctl_dir(struct file *file, unsigned int cmd,
 		    unsigned long arg __maybe_unused)
 {
 	long err;
-	struct super_block *sb;
-	struct au_sbinfo *sbinfo;
 
-	err = -EACCES;
-	if (!capable(CAP_SYS_ADMIN))
-		goto out;
-
-	err = 0;
-	sb = file->f_dentry->d_sb;
-	sbinfo = au_sbi(sb);
 	switch (cmd) {
 	case AUFS_CTL_PLINK_MAINT:
-		/*
-		 * pseudo-link maintenance mode,
-		 * cleared by aufs_release_dir()
-		 */
-		si_write_lock(sb);
-		if (!au_ftest_si(sbinfo, MAINTAIN_PLINK)) {
-			au_fset_si(sbinfo, MAINTAIN_PLINK);
-			au_fi(file)->fi_maintain_plink = 1;
-		} else
-			err = -EBUSY;
-		si_write_unlock(sb);
-		break;
 	case AUFS_CTL_PLINK_CLEAN:
-		aufs_write_lock(sb->s_root);
-		if (au_opt_test(sbinfo->si_mntflags, PLINK))
-			au_plink_put(sb);
-		aufs_write_unlock(sb->s_root);
+		err = au_plink_ioctl(file, cmd);
 		break;
 	default:
 		err = -EINVAL;
 	}
 
- out:
 	return err;
 }
