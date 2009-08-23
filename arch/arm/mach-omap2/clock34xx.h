fd13d82ce24b1f0113d934fd6ec090b30dcc097c
Girish S G <girishsg@ti.com>
2009-08-22 21:17:55 -0500

    [ARM] omap2: add set_rate handler to sgx clock
    Change-Id: If9365bb57b39ab0f73c33283d6c5431a75e53a93
    Signed-off-by: Girish S G <girishsg@ti.com>

--- arch/arm/mach-omap2/clock34xx.h
+++ arch/arm/mach-omap2/clock34xx.h
@@ -1203,6 +1203,7 @@ static struct clk sgx_fck = {
 	.clksel		= sgx_clksel,
 	.clkdm_name	= "sgx_clkdm",
 	.recalc		= &omap2_clksel_recalc,
+	.set_rate	= &omap2_clksel_set_rate,
 };
 
 static struct clk sgx_ick = {
