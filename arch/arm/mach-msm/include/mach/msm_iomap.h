7f5f1374e4e5f556b9278aec3decce1840629ea0
Dima Zavin <dima@android.com>
2009-09-15 14:16:20 -0700

    [ARM] msm: Remove unused regions that were tagged as remapped in iomap.h
    These defines were marked as if iomem was being reserved for them at
    initialization, but they were not actually in the msm_io_desc list in
    io.c, and no users for these defines exist.
    
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -112,7 +112,6 @@
 #define MSM_I2C_SIZE          SZ_4K
 
 #define MSM_HSUSB_PHYS        0xA0800000
-#define MSM_HSUSB_BASE        IOMEM(0xE0009000)
 #define MSM_HSUSB_SIZE        SZ_4K
 
 #define MSM_PMDH_PHYS         0xAA600000
@@ -133,18 +132,15 @@
 #define MSM_AD5_PHYS          0xAC000000
 #define MSM_AD5_SIZE          (SZ_1M*13)
 
-#define MSM_VFE_BASE          IOMEM(0xE1100000)
 #define MSM_VFE_PHYS          0xA0F00000
 #define MSM_VFE_SIZE          SZ_1M
 
 #define MSM_UART1DM_PHYS      0xA0200000
 #define MSM_UART2DM_PHYS      0xA0300000
 
-#define MSM_SSBI_BASE         IOMEM(0xE1004000)
 #define MSM_SSBI_PHYS         0xA8100000
 #define MSM_SSBI_SIZE         SZ_4K
 
-#define MSM_TSSC_BASE         IOMEM(0xE1005000)
 #define MSM_TSSC_PHYS         0xAA300000
 #define MSM_TSSC_SIZE         SZ_4K
 
