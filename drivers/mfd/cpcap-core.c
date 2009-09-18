4a3eedb9756e866b92b7c8c0473631604faa862a
Jason Knopsnyder <w19069@motorola.com>
2009-09-18 12:08:55 -0500

    mfd: cpcap: Disable the CPCAP transceiver on powerdown
    This fixes the following scenario:
    1). Phone powered up with a USB cable attached and enumerated.
    2). Send the reboot command via adb
    3). While powering down due to the reboot, the phone will de-enumerate.
    4). A moment later (while still powering down for the reboot) the phone will
     re-enumerate.
    5). The phone then power down and restart.
    
    By disabling the CPCAP transceiver, the re-enumeration will not occur. This
    re-enumeration is causing havoc with automated tests that see the
    re-enumeration and decide the reboot has completed.
    
    Change-Id: I3f426443dbbcaf6019518561cabcfeb1da927020
    Signed-off-by: Jason Knopsnyder <w19069@motorola.com>

--- drivers/mfd/cpcap-core.c
+++ drivers/mfd/cpcap-core.c
@@ -175,6 +175,16 @@ static int cpcap_reboot(struct notifier_block *this, unsigned long code,
 	int result = NOTIFY_DONE;
 	char *mode = cmd;
 
+	/* Disable the USB transceiver */
+	ret = cpcap_regacc_write(misc_cpcap, CPCAP_REG_USBC2, 0,
+				 CPCAP_BIT_USBXCVREN);
+
+	if (ret) {
+		dev_err(&(misc_cpcap->spi->dev),
+			"Disable Transciever failure.\n");
+		result = NOTIFY_BAD;
+	}
+
 	if (code == SYS_RESTART) {
 		/* Set the soft reset bit in the cpcap */
 		ret = cpcap_regacc_write(misc_cpcap, CPCAP_REG_VAL1,
