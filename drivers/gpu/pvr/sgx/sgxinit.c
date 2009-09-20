ce005fd2677f66317d01ae1d5ec1b7f98e6633b6
Rebecca Schultz Zavin <rebecca@android.com>
2009-09-20 16:18:00 -0700

    gpu: pvr: In wait for hardware to be ready, check condition on loop exit
    Without this change (and with preemption) it is possible to exit the loop
    because the timer has expired with the flag set to FALSE even though
    the condition is now true.  Instead of checking the flag, check the
    condition again before returning an error.
    
    Change-Id: I29a59d718c73e7e0a378e26f86c630dfa70a7764
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/gpu/pvr/sgx/sgxinit.c
+++ drivers/gpu/pvr/sgx/sgxinit.c
@@ -1578,25 +1578,15 @@ PVRSRV_ERROR SGXGetBuildInfoKM(PVRSRV_SGXDEV_INFO	*psDevInfo,
 
 	
 #if !defined(NO_HARDWARE)
+	LOOP_UNTIL_TIMEOUT(MAX_HW_TIME_US)
 	{
-		IMG_BOOL bExit;
-		
-		bExit = IMG_FALSE;
-		LOOP_UNTIL_TIMEOUT(MAX_HW_TIME_US)
-		{
-			if ((psSGXMiscInfoInt->ui32MiscInfoFlags & PVRSRV_USSE_MISCINFO_READY) != 0)
-			{
-				bExit = IMG_TRUE;
-				break;
-			}
-		} END_LOOP_UNTIL_TIMEOUT();
-		
-		
-		if (!bExit)
-		{
-			return PVRSRV_ERROR_TIMEOUT;
-		}
-	}
+		if ((psSGXMiscInfoInt->ui32MiscInfoFlags & PVRSRV_USSE_MISCINFO_READY) != 0)
+			break;
+	} END_LOOP_UNTIL_TIMEOUT();
+
+
+	if (!(psSGXMiscInfoInt->ui32MiscInfoFlags & PVRSRV_USSE_MISCINFO_READY))
+		return PVRSRV_ERROR_TIMEOUT;
 #endif 
 
 	return PVRSRV_OK;
