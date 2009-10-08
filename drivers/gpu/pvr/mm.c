71897d16aa8f260ff2e702b42b502881366c1bbf
Rebecca Schultz Zavin <rebecca@android.com>
2009-10-07 17:38:15 -0700

    gpu: pvr: Really flush the cache when uncached memory is allocated
    Change-Id: Ida1209b9d56c4ed6c1efdb485f7010555277ef9f
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/gpu/pvr/mm.c
+++ drivers/gpu/pvr/mm.c
@@ -572,7 +572,8 @@ _VMallocWrapper(IMG_UINT32 ui32Bytes,
     }
 
     pvRet = __vmalloc(ui32Bytes, GFP_KERNEL | __GFP_HIGHMEM, PGProtFlags);
-    if (PGProtFlags == PVRSRV_HAP_WRITECOMBINE || PGProtFlags == PVRSRV_HAP_UNCACHED)
+    if ((ui32AllocFlags & PVRSRV_HAP_CACHETYPE_MASK) == PVRSRV_HAP_WRITECOMBINE ||
+	(ui32AllocFlags & PVRSRV_HAP_CACHETYPE_MASK) == PVRSRV_HAP_UNCACHED)
 	    flush_cache_all();
 
 #if defined(DEBUG_LINUX_MEMORY_ALLOCATIONS)
