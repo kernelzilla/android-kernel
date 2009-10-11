9244d65deeb660d0096ced7d3b884ab26cf131c2
Rebecca Schultz Zavin <rebecca@android.com>
2009-10-10 18:59:18 -0700

    gpu: pvr: Check the condition after the timeout.
    Without the check after the timeout the function can return false
    because it was preempted and the timeout expired.
    
    Change-Id: I1f7c503d1c34572e22d11e2e13056f506d381c60
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/gpu/pvr/event.c
+++ drivers/gpu/pvr/event.c
@@ -240,6 +240,7 @@ PVRSRV_ERROR LinuxEventObjectWait(IMG_HANDLE hOSEventObject, IMG_UINT32 ui32MSTi
 	PVRSRV_LINUX_EVENT_OBJECT *psLinuxEventObject = (PVRSRV_LINUX_EVENT_OBJECT *) hOSEventObject;
 
 	IMG_UINT32 ui32TimeOutJiffies = msecs_to_jiffies(ui32MSTimeout);
+	PVRSRV_ERROR ret = PVRSRV_ERROR_TIMEOUT;
 	
 	do	
 	{
@@ -248,6 +249,7 @@ PVRSRV_ERROR LinuxEventObjectWait(IMG_HANDLE hOSEventObject, IMG_UINT32 ui32MSTi
    	
 		if(psLinuxEventObject->ui32TimeStampPrevious != ui32TimeStamp)
 		{
+			ret = PVRSRV_OK;
 			break;
 		}
 
@@ -260,14 +262,17 @@ PVRSRV_ERROR LinuxEventObjectWait(IMG_HANDLE hOSEventObject, IMG_UINT32 ui32MSTi
 		psLinuxEventObject->ui32Stats++;
 #endif			
 
-		
 	} while (ui32TimeOutJiffies);
 
-	finish_wait(&psLinuxEventObject->sWait, &sWait);	
+	finish_wait(&psLinuxEventObject->sWait, &sWait);
+
+	ui32TimeStamp = atomic_read(&psLinuxEventObject->sTimeStamp);
+	if(psLinuxEventObject->ui32TimeStampPrevious != ui32TimeStamp)
+		ret = PVRSRV_OK;
 
 	psLinuxEventObject->ui32TimeStampPrevious = ui32TimeStamp;
 
-	return ui32TimeOutJiffies ? PVRSRV_OK : PVRSRV_ERROR_TIMEOUT;
+	return ret;
 
 }
 
