ed71f3d462a549e3de24a6dfcd16ff853ff09dc1
Russell King <rmk+kernel@arm.linux.org.uk>
2009-10-31 16:07:16 +0000

    ARM: dma-mapping: provide dma_to_page()
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Tested-By: Jamie Iles <jamie@jamieiles.com>

--- arch/arm/include/asm/dma-mapping.h
+++ arch/arm/include/asm/dma-mapping.h
@@ -30,6 +30,11 @@ static inline dma_addr_t page_to_dma(struct device *dev, struct page *page)
 #error "this machine class needs to define __arch_page_to_dma to use HIGHMEM"
 #endif
 
+static inline struct page *dma_to_page(struct device *dev, dma_addr_t addr)
+{
+	return pfn_to_page(__bus_to_pfn(addr));
+}
+
 static inline void *dma_to_virt(struct device *dev, dma_addr_t addr)
 {
 	return (void *)__bus_to_virt(addr);
@@ -43,6 +48,11 @@ static inline dma_addr_t virt_to_dma(struct device *dev, void *addr)
 static inline dma_addr_t page_to_dma(struct device *dev, struct page *page)
 {
 	return __arch_page_to_dma(dev, page);
+}
+
+static inline struct page *dma_to_page(struct device *dev, dma_addr_t addr)
+{
+	return __arch_dma_to_page(dev, addr);
 }
 
 static inline void *dma_to_virt(struct device *dev, dma_addr_t addr)
