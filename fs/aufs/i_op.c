1fb16b3a49bad5106bb063dbc1955571ce414153
J. R. Okajima <hooanon05@yahoo.co.jp>
2009-12-07 21:08:15 +0900

    aufs: linux-2.6.33-rcN, call security_path_{chown,chmod}()
    In linux-2.6.33-rcN, some new path-baseed security functions are
    introduced.
    
    Signed-off-by: J. R. Okajima <hooanon05@yahoo.co.jp>

--- fs/aufs/i_op.c
+++ fs/aufs/i_op.c
@@ -603,6 +603,19 @@ static int aufs_setattr(struct dentry *dentry, struct iattr *ia)
 	}
 
 	a->h_path.mnt = au_sbr_mnt(sb, a->btgt);
+	if ((ia->ia_valid & (ATTR_MODE | ATTR_CTIME))
+	    == (ATTR_MODE | ATTR_CTIME)) {
+		err = security_path_chmod(a->h_path.dentry, a->h_path.mnt,
+					  ia->ia_mode);
+		if (unlikely(err))
+			goto out_unlock;
+	} else if ((ia->ia_valid & (ATTR_UID | ATTR_GID))
+		   && (ia->ia_valid & ATTR_CTIME)) {
+		err = security_path_chown(&a->h_path, ia->ia_uid, ia->ia_gid);
+		if (unlikely(err))
+			goto out_unlock;
+	}
+
 	if (ia->ia_valid & ATTR_SIZE) {
 		struct file *f;
 
