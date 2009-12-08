69bde14482ce2b7d68b8266aa2d376d890793489
Rebecca Schultz Zavin <rebecca@android.com>
2009-12-07 17:30:56 -0800

    [ARM] omap2: sdram: Add sdram parameter file for toshiba/hynix/numonyx part
    Submitted on behalf of Limei Wang <E12499@motorola.com>
    Change-Id: I1ca1fa5b9e1e11d28bda3cc5ac583ea25d6ea97f
    Signed-off-by: Jared Suttles <jared.suttles@motorola.com>
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

---
+++ arch/arm/mach-omap2/sdram-toshiba-hynix-numonyx.h
@@ -0,0 +1,37 @@
+/*
+ * SDRC register values for the Toshiba, Hynxi and Numonyx.
+ * These are common SDRC parameters for all vendors since we use the JEDEC JESD209A parameters.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef ARCH_ARM_MACH_OMAP2_SDRAM_TOSHIBA_HYNIX_NUMONYX
+
+#define ARCH_ARM_MACH_OMAP2_SDRAM_TOSHIBA_HYNIX_NUMONYX
+
+#include <plat/sdrc.h>
+
+static struct omap_sdrc_params JEDEC_JESD209A_sdrc_params[] = {
+    [0] = {
+        .rate        = 160500000,
+        .actim_ctrla = 0xBA9DB4C6,
+        .actim_ctrlb = 0x00022221,
+        .rfr_ctrl    = 0x0004B902,
+        .mr		     = 0x00000032,
+    },
+    [1] = {
+        .rate        = 80250000,
+        .actim_ctrla = 0x49512284,
+        .actim_ctrlb = 0x0001120C,
+        .rfr_ctrl    = 0x23F02,
+        .mr		     = 0x00000032,
+    },
+    [2] = {
+        .rate        = 0
+    },
+};
+
+#endif
+
