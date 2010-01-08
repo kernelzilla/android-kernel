2faa14554e5a807cc8f4409713c622d9a1019762
Rebecca Schultz Zavin <rebecca@android.com>
2010-01-07 18:45:36 -0800

    DSS2: dsi: If an error is encountered, reinitialize the dsi link
    Change-Id: I76037aab16bfc5fb5c0e0db87e17fd04c5dbdc54
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/video/omap2/dss/dsi.c
+++ drivers/video/omap2/dss/dsi.c
@@ -265,6 +265,13 @@ static struct
 #endif
 	int debug_read;
 	int debug_write;
+
+	struct {
+		struct work_struct work;
+		struct omap_dss_device *dssdev;
+		bool enabled;
+		bool recovering;
+	} error_recovery;
 } dsi;
 
 #ifdef DEBUG
@@ -527,6 +534,12 @@ static void print_irq_status_cio(u32 status)
 
 static int debug_irq;
 
+static void schedule_error_recovery(void)
+{
+	if (dsi.error_recovery.enabled && !dsi.error_recovery.recovering)
+		schedule_work(&dsi.error_recovery.work);
+}
+
 /* called from dss */
 void dsi_irq_handler(void)
 {
@@ -541,6 +554,10 @@ void dsi_irq_handler(void)
 		spin_lock(&dsi.errors_lock);
 		dsi.errors |= irqstatus & DSI_IRQ_ERROR_MASK;
 		spin_unlock(&dsi.errors_lock);
+		if (irqstatus & DSI_IRQ_TA_TIMEOUT)
+		{
+			schedule_error_recovery();
+		}
 	} else if (debug_irq) {
 		print_irq_status(irqstatus);
 	}
@@ -562,6 +579,11 @@ void dsi_irq_handler(void)
 		if (vcstatus & DSI_VC_IRQ_ERROR_MASK) {
 			DSSERR("DSI VC(%d) error, vc irqstatus %x\n",
 				       i, vcstatus);
+			if (vcstatus & DSI_VC_IRQ_BTA)
+			{
+				schedule_error_recovery();
+			}
+
 			print_irq_status_vc(i, vcstatus);
 		} else if (debug_irq) {
 			print_irq_status_vc(i, vcstatus);
@@ -1823,6 +1845,7 @@ static u16 dsi_vc_flush_receive_data(int channel)
 		if (dt == DSI_DT_RX_ACK_WITH_ERR) {
 			u16 err = FLD_GET(val, 23, 8);
 			dsi_show_rx_ack_with_err(err);
+			schedule_error_recovery();
 		} else if (dt == DSI_DT_RX_SHORT_READ_1) {
 			DSSDBG("\tDCS short response, 1 byte: %#x\n",
 					FLD_GET(val, 23, 8));
@@ -3263,6 +3286,7 @@ static int dsi_display_enable(struct omap_dss_device *dssdev)
 
 	enable_clocks(0);
 	dsi_enable_pll_clock(0);
+	dsi.error_recovery.enabled = true;
 
 	dsi_bus_unlock();
 	mutex_unlock(&dsi.lock);
@@ -3293,6 +3317,8 @@ static void dsi_display_disable(struct omap_dss_device *dssdev)
 	mutex_lock(&dsi.lock);
 	dsi_bus_lock();
 
+	dsi.error_recovery.enabled = false;
+
 	if (dssdev->state == OMAP_DSS_DISPLAY_DISABLED ||
 			dssdev->state == OMAP_DSS_DISPLAY_SUSPENDED)
 		goto end;
@@ -3322,6 +3348,8 @@ static int dsi_display_suspend(struct omap_dss_device *dssdev)
 
 	mutex_lock(&dsi.lock);
 	dsi_bus_lock();
+
+	dsi.error_recovery.enabled = false;
 
 	if (dssdev->state == OMAP_DSS_DISPLAY_DISABLED ||
 			dssdev->state == OMAP_DSS_DISPLAY_SUSPENDED)
@@ -3388,6 +3416,7 @@ static int dsi_display_resume(struct omap_dss_device *dssdev)
 
 	enable_clocks(0);
 	dsi_enable_pll_clock(0);
+	dsi.error_recovery.enabled = true;
 
 	dsi_bus_unlock();
 	mutex_unlock(&dsi.lock);
@@ -3720,7 +3749,54 @@ int dsi_init_display(struct omap_dss_device *dssdev)
 	dsi.vc[0].dssdev = dssdev;
 	dsi.vc[1].dssdev = dssdev;
 
+	dsi.error_recovery.dssdev = dssdev;
+
 	return 0;
+}
+
+static void dsi_error_recovery_worker(struct work_struct *work)
+{
+	u32 r;
+	DSSERR("DSI error, ESD detected\n");
+
+	mutex_lock(&dsi.lock);
+
+	if (!dsi.error_recovery.enabled)
+               goto end;
+
+	dsi_bus_lock();
+
+	dsi.error_recovery.recovering = true;
+
+	enable_clocks(1);
+	dsi_enable_pll_clock(1);
+	dsi_force_tx_stop_mode_io();
+
+	r = dsi_read_reg(DSI_TIMING1);
+	r = FLD_MOD(r, 0, 15, 15);	/* FORCE_TX_STOP_MODE_IO */
+	dsi_write_reg(DSI_TIMING1, r);
+
+	dsi_vc_enable(0, 0);
+	dsi_vc_enable(1, 0);
+	dsi_if_enable(0);
+
+	dsi_vc_enable(0, 1);
+	dsi_vc_enable(1, 1);
+	dsi_if_enable(1);
+
+	dsi_force_tx_stop_mode_io();
+
+	dsi.error_recovery.recovering = false;
+
+	if (dsi.update_mode == OMAP_DSS_UPDATE_AUTO)
+		dsi_start_auto_update(dsi.error_recovery.dssdev);
+	enable_clocks(0);
+	dsi_enable_pll_clock(0);
+
+	dsi_bus_unlock();
+
+end:
+	mutex_unlock(&dsi.lock);
 }
 
 int dsi_init(struct platform_device *pdev)
@@ -3756,6 +3832,11 @@ int dsi_init(struct platform_device *pdev)
 	dsi.te_timer.function = dsi_te_timeout;
 	dsi.te_timer.data = 0;
 #endif
+
+	dsi.error_recovery.dssdev = 0;
+	dsi.error_recovery.enabled = false;
+	dsi.error_recovery.recovering = false;
+	INIT_WORK(&dsi.error_recovery.work, dsi_error_recovery_worker);
 
 	dsi.update_mode = OMAP_DSS_UPDATE_DISABLED;
 	dsi.user_update_mode = OMAP_DSS_UPDATE_DISABLED;
