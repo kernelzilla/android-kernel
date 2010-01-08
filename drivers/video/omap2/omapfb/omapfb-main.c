b627b759dc218777d15cb5b3c689d017ee65c959
Rebecca Schultz Zavin <rebecca@android.com>
2010-01-07 19:02:57 -0800

    DSS2: suspend and resume handlers in fb driver
    Change-Id: I9e7980564f6542b195ef4d8905b0e62eec92e54a

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -1702,14 +1702,10 @@ void suspend(struct early_suspend *h)
 	struct suspend_info *info = container_of(h, struct suspend_info,
 						early_suspend);
 	struct fb_info *fbi = info->fbi;
-	struct omapfb_info *ofbi = FB2OFB(fbi);
-	struct omapfb2_device *fbdev = ofbi->fbdev;
 	struct omap_dss_device *display = fb2display(fbi);
 
 	if (display->suspend)
 		display->suspend(display);
-
-	omapfb_vrfb_suspend_all(fbdev);
 }
 
 void resume(struct early_suspend *h)
@@ -1717,11 +1713,7 @@ void resume(struct early_suspend *h)
 	struct suspend_info *info = container_of(h, struct suspend_info,
 						early_suspend);
 	struct fb_info *fbi = info->fbi;
-	struct omapfb_info *ofbi = FB2OFB(fbi);
-	struct omapfb2_device *fbdev = ofbi->fbdev;
 	struct omap_dss_device *display = fb2display(fbi);
-
-	omapfb_vrfb_resume_all(fbdev);
 
 	if (display->resume)
 		display->resume(display);
