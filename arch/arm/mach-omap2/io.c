4a80c817692b22a31fff66523fe49661553cd3ac
Rebecca Schultz Zavin <rebecca@android.com>
2010-01-26 16:12:41 -0800

    DSPBRIDGE: Add the omap34XX dspbridge code
    Change-Id: Id334acb2ee98dc9815ac31ff630244eb461af500

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -50,6 +50,8 @@
 #include "omap_hwmod_2430.h"
 #include "omap_hwmod_34xx.h"
 
+#include <dspbridge/host_os.h>
+
 /*
  * The machine specific code may provide the extra mapping besides the
  * default mapping provided here.
@@ -266,6 +268,7 @@ void __init omap2_map_common_io(void)
 	omap_sram_init();
 	omapfb_reserve_sdram();
 	omap_vram_reserve_sdram();
+	dspbridge_reserve_sdram();
 }
 
 /*
