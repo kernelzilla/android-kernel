b6ce3586965e02b9fd9cb407e580ac0f70354176
Fernando Guzman Lugo <x0095840@ti.com>
2010-02-05 01:35:35 -0600

    DSPBRIDGE: cache against kernel address fix
    Replace page_address with kmap to get address in case of high mem allocation.
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>

--- drivers/dsp/bridge/rmgr/proc.c
+++ drivers/dsp/bridge/rmgr/proc.c
@@ -680,11 +680,12 @@ static int memory_sync_page(struct vm_area_struct *vma, unsigned long start,
 		}
 
 		offset = start & ~PAGE_MASK;
-		kaddr = page_address(page) + offset;
+		kaddr = kmap(page) + offset;
 		rest = min_t(ssize_t, PAGE_SIZE - offset, len);
 
 		MEM_FlushCache(kaddr, rest, ftype);
 
+		kunmap(page);
 		put_page(page);
 		len -= rest;
 		start += rest;
