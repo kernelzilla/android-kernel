8c197ba6fabd00e7c9b8abd9afa7b61ad455f708
Rebecca Schultz Zavin <rebecca@android.com>
2010-02-08 16:03:38 -0800

    [ARM] omap2: Set SRFRONRESET bit in SDRC_POWER It allows SDRAM enter self-refresh state in case warm reset
    It is needed for features(like ramconsole)
    that rely on valid SDRAM content even after a warm reset
    
    Change-Id: I3f44f9cc5b1860dd0e71bc666dd763080ed55c1e
    Signed-off-by: Tao Hu <taohu@motorola.com>
    Signed-off-by: San Mehat <san@google.com>
    
    Conflicts:
    
    	arch/arm/mach-omap2/sdrc.c

--- arch/arm/mach-omap2/sdrc.c
+++ arch/arm/mach-omap2/sdrc.c
@@ -44,6 +44,7 @@ struct omap2_sms_regs {
 static struct omap2_sms_regs sms_context;
 
 /* SDRC_POWER register bits */
+#define SDRC_POWER_SRFRONRESET			7
 #define SDRC_POWER_EXTCLKDIS_SHIFT		3
 #define SDRC_POWER_PWDENA_SHIFT			2
 #define SDRC_POWER_PAGEPOLICY_SHIFT		0
@@ -155,7 +156,8 @@ void __init omap2_sdrc_init(struct omap_sdrc_params *sdrc_cs0,
 	 * PWDENA should not be set due to 34xx erratum 1.150 - PWDENA
 	 * can cause random memory corruption
 	 */
-	l = (1 << SDRC_POWER_EXTCLKDIS_SHIFT) |
+	l = (1 << SDRC_POWER_SRFRONRESET) |
+		(1 << SDRC_POWER_EXTCLKDIS_SHIFT) |
 		(1 << SDRC_POWER_PAGEPOLICY_SHIFT);
 	sdrc_write_reg(l, SDRC_POWER);
 	omap2_sms_save_context();
