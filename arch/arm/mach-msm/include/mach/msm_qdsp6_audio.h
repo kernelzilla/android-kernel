0290a72bd7fcab585ba0c8c63d3b75938ff307cc
Brian Swetland <swetland@google.com>
2010-02-24 16:13:20 -0800

    qsd8k: debugging / ramdump driver for QDSP6
    Provide a mechanism for userspace to get notification of DSP crashes,
    and to extract the DSP's RAM image before allowing the crash to "complete".
    
    Change-Id: Id091c187307510999a8b3ea614c17c3268c2036c
    Signed-off-by: Brian Swetland <swetland@google.com>
    Signed-off-by: Ashraya Arya <ashrayaa@quicinc.com>

--- arch/arm/mach-msm/include/mach/msm_qdsp6_audio.h
+++ arch/arm/mach-msm/include/mach/msm_qdsp6_audio.h
@@ -101,4 +101,7 @@ struct q6audio_analog_ops {
 
 void q6audio_register_analog_ops(struct q6audio_analog_ops *ops);
 
+/* signal non-recoverable DSP error so we can log and/or panic */
+void q6audio_dsp_not_responding(void);
+
 #endif
