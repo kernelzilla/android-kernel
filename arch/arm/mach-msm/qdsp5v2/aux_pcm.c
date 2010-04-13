798f563f1358c1bd0dfedc4c759eb8a0c50dc689
Helen Zeng <xiaoyunz@quicinc.com>
2010-04-12 17:44:39 -0700

    msm: audio: qdsp5v2: Prevent buzzing sound in Bluetooth device at call end.
    Feed silence frames to the Bluetooth device before close to prevent
    buzzing sound. This fix is applicable only to Marimba Bluetooth.
    
    Change-Id: I4935c40412a4bbf02969f8bddf453df599c8d7bd
    CRs-Fixed: 224761
    Signed-off-by: Helen Zeng <xiaoyunz@quicinc.com>

--- arch/arm/mach-msm/qdsp5v2/aux_pcm.c
+++ arch/arm/mach-msm/qdsp5v2/aux_pcm.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -23,6 +23,7 @@
 #include <linux/err.h>
 #include <mach/qdsp5v2/aux_pcm.h>
 #include <mach/gpio.h>
+#include <linux/delay.h>
 
 /*----------------------------------------------------------------------------
  * Preprocessor Definitions and Constants
@@ -158,6 +159,17 @@ EXPORT_SYMBOL(aux_pcm_gpios_request);
 void aux_pcm_gpios_free(void)
 {
 	MM_INFO(" aux_pcm_gpios_free \n");
+
+	/*
+	 * Feed silence frames before close to prevent buzzing sound in BT at
+	 * call end. This fix is applicable only to Marimba BT.
+	 */
+	gpio_tlmm_config(GPIO_CFG(the_aux_pcm_state.dout, 0, GPIO_OUTPUT,
+		GPIO_NO_PULL, GPIO_2MA), GPIO_ENABLE);
+	gpio_set_value(the_aux_pcm_state.dout, 0);
+	msleep(20);
+	gpio_tlmm_config(GPIO_CFG(the_aux_pcm_state.dout, 1, GPIO_OUTPUT,
+		GPIO_NO_PULL, GPIO_2MA), GPIO_ENABLE);
 
 	qcom_gpio_free(the_aux_pcm_state.dout);
 	qcom_gpio_free(the_aux_pcm_state.din);
