8bbb084294c576722b05e5d941528f5cf129bffd
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2010-04-14 12:42:37 -0700

    msm: display: backlight intensity
    Update the panel driver to handle backlight intensity calls
    using pmic's pwm channels.
    
    Change-Id: I3c63588e5244748da5461e43f7f0831ddf635c5c
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/video/msm/lcdc_samsung_wsvga.c
+++ drivers/video/msm/lcdc_samsung_wsvga.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -15,50 +15,169 @@
  * 02110-1301, USA.
  */
 
+#include <linux/delay.h>
+#include <linux/pwm.h>
+#ifdef CONFIG_PMIC8058_PWM
+#include <linux/mfd/pmic8058.h>
+#include <linux/pmic8058-pwm.h>
+#endif
+#include <mach/gpio.h>
 #include "msm_fb.h"
 
-#ifdef CONFIG_FB_MSM_TRY_MDDI_CATCH_LCDC_PRISM
-#include "mddihosti.h"
+
+
+#ifdef CONFIG_PMIC8058_PWM
+static struct pwm_device *bl_pwm0;
+static struct pwm_device *bl_pwm1;
+
+/* 50 Khz == 20000 ns period
+ * divide 20000 ns to 15 levels
+ * each level has 1333 ns
+ */
+
+/* for samsung panel 300hz was the minimum freq where flickering wasnt
+ * observed as the screen was dimmed
+ */
+
+#define PWM_FREQ_HZ 300
+#define PWM_PERIOD (NSEC_PER_SEC / PWM_FREQ_HZ)
+#define PWM_LEVEL 15
+#define PWM_DUTY_LEVEL (PWM_PERIOD / PWM_LEVEL)
 #endif
 
-static int __init lcdc_prism_init(void)
+static struct msm_panel_common_pdata *lcdc_samsung_pdata;
+
+
+static void lcdc_samsung_panel_set_backlight(struct msm_fb_data_type *mfd)
+{
+	int bl_level;
+	int ret;
+
+	bl_level = mfd->bl_level;
+
+#ifdef CONFIG_PMIC8058_PWM
+	if (bl_pwm0) {
+		ret = pwm_config(bl_pwm0, PWM_DUTY_LEVEL * bl_level,
+			PWM_PERIOD);
+		if (ret)
+			printk(KERN_ERR "pwm_config on pwm 0 failed %d\n", ret);
+	}
+
+	if (bl_pwm1) {
+		ret = pwm_config(bl_pwm1,
+			PWM_PERIOD - (PWM_DUTY_LEVEL * bl_level),
+			PWM_PERIOD);
+		if (ret)
+			printk(KERN_ERR "pwm_config on pwm 1 failed %d\n", ret);
+	}
+
+	if (bl_pwm0) {
+		ret = pwm_enable(bl_pwm0);
+		if (ret)
+			printk(KERN_ERR "pwm_enable on pwm 0 failed %d\n", ret);
+	}
+
+	if (bl_pwm1) {
+		ret = pwm_enable(bl_pwm1);
+		if (ret)
+			printk(KERN_ERR "pwm_enable on pwm 1 failed %d\n", ret);
+	}
+#endif
+
+}
+
+static int __init samsung_probe(struct platform_device *pdev)
+{
+	if (pdev->id == 0) {
+		lcdc_samsung_pdata = pdev->dev.platform_data;
+		return 0;
+	}
+
+#ifdef CONFIG_PMIC8058_PWM
+	bl_pwm0 = pwm_request(lcdc_samsung_pdata->gpio_num[0], "backlight1");
+	if (bl_pwm0 == NULL || IS_ERR(bl_pwm0)) {
+		pr_err("%s pwm_request() failed\n", __func__);
+		bl_pwm0 = NULL;
+	}
+
+	bl_pwm1 = pwm_request(lcdc_samsung_pdata->gpio_num[1], "backlight2");
+	if (bl_pwm1 == NULL || IS_ERR(bl_pwm1)) {
+		pr_err("%s pwm_request() failed\n", __func__);
+		bl_pwm1 = NULL;
+	}
+
+	printk(KERN_INFO "samsung_probe: bl_pwm0=%p LPG_chan0=%d "
+			"bl_pwm1=%p LPG_chan1=%d\n",
+			bl_pwm0, (int)lcdc_samsung_pdata->gpio_num[0],
+			bl_pwm1, (int)lcdc_samsung_pdata->gpio_num[1]
+			);
+#endif
+
+	msm_fb_add_device(pdev);
+
+	return 0;
+}
+
+static struct platform_driver this_driver = {
+	.probe  = samsung_probe,
+	.driver = {
+		.name   = "lcdc_samsung_wsvga",
+	},
+};
+
+static struct msm_fb_panel_data samsung_panel_data = {
+	.set_backlight = lcdc_samsung_panel_set_backlight,
+};
+
+static struct platform_device this_device = {
+	.name   = "lcdc_samsung_wsvga",
+	.id	= 1,
+	.dev	= {
+		.platform_data = &samsung_panel_data,
+	}
+};
+
+static int __init lcdc_samsung_panel_init(void)
 {
 	int ret;
-	struct msm_panel_info pinfo;
+	struct msm_panel_info *pinfo;
 
-#ifdef CONFIG_FB_MSM_TRY_MDDI_CATCH_LCDC_PRISM
-	ret = msm_fb_detect_client("lcdc_samsung_wsvga");
-	if (ret == -ENODEV)
-		return 0;
-
-	if (ret && (mddi_get_client_id() != 0))
+#ifdef CONFIG_FB_MSM_MDDI_AUTO_DETECT
+	if (msm_fb_detect_client("lcdc_samsung_wsvga"))
 		return 0;
 #endif
 
-	pinfo.xres = 1024;
-	pinfo.yres = 600;
-	pinfo.type = LCDC_PANEL;
-	pinfo.pdest = DISPLAY_1;
-	pinfo.wait_cycle = 0;
-	pinfo.bpp = 18;
-	pinfo.fb_num = 2;
-	pinfo.clk_rate = 53990000;
-
-	pinfo.lcdc.h_back_porch = 80;
-	pinfo.lcdc.h_front_porch = 48;
-	pinfo.lcdc.h_pulse_width = 32;
-	pinfo.lcdc.v_back_porch = 4;
-	pinfo.lcdc.v_front_porch = 3;
-	pinfo.lcdc.v_pulse_width = 1;
-	pinfo.lcdc.border_clr = 0;	/* blk */
-	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
-	pinfo.lcdc.hsync_skew = 0;
-
-	ret = lcdc_device_register(&pinfo);
+	ret = platform_driver_register(&this_driver);
 	if (ret)
-		printk(KERN_ERR "%s: failed to register device!\n", __func__);
+		return ret;
+
+	pinfo = &samsung_panel_data.panel_info;
+	pinfo->xres = 1024;
+	pinfo->yres = 600;
+	pinfo->type = LCDC_PANEL;
+	pinfo->pdest = DISPLAY_1;
+	pinfo->wait_cycle = 0;
+	pinfo->bpp = 18;
+	pinfo->fb_num = 2;
+	pinfo->clk_rate = 53990000;
+	pinfo->bl_max = 15;
+	pinfo->bl_min = 1;
+
+	pinfo->lcdc.h_back_porch = 80;
+	pinfo->lcdc.h_front_porch = 48;
+	pinfo->lcdc.h_pulse_width = 32;
+	pinfo->lcdc.v_back_porch = 4;
+	pinfo->lcdc.v_front_porch = 3;
+	pinfo->lcdc.v_pulse_width = 1;
+	pinfo->lcdc.border_clr = 0;
+	pinfo->lcdc.underflow_clr = 0xff;
+	pinfo->lcdc.hsync_skew = 0;
+
+	ret = platform_device_register(&this_device);
+	if (ret)
+		platform_driver_unregister(&this_driver);
 
 	return ret;
 }
 
-module_init(lcdc_prism_init);
+module_init(lcdc_samsung_panel_init);
