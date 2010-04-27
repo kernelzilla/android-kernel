aaf28137c2900b7c59b2312b42a9a10c740f56b9
Steve Muckle <smuckle@quicinc.com>
2010-04-27 14:34:08 -0700

    msm: do not select gpiolib
    MSM does not yet use gpiolib in all configurations.
    
    Change-Id: I948f6f869087b384d5bf6f450e2fcbf149abd89d
    Signed-off-by: Steve Muckle <smuckle@quicinc.com>

--- arch/arm/mach-msm/include/mach/gpio-v1.h
+++ arch/arm/mach-msm/include/mach/gpio-v1.h
@@ -84,11 +84,6 @@ int msm_gpios_enable(const struct msm_gpio *table, int size);
  */
 void msm_gpios_disable(const struct msm_gpio *table, int size);
 
-static inline int gpio_cansleep(unsigned gpio)
-{
-	return 0;
-}
-
 int qcom_gpio_request(unsigned gpio, const char *label);
 void qcom_gpio_free(unsigned gpio);
 int qcom_gpio_direction_input(unsigned gpio);
