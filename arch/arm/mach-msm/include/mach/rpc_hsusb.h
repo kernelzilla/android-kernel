a630d104f12ed787fe3c22c343e4356974d65166
Vamsi Krishna <vskrishn@quicinc.com>
2010-05-05 16:40:08 -0700

    usb: msm_gadget: Stub out rpc apis for 8660
    For 8660 smd rpc router is not up yet.  Stub out rpc calls
    when rpc router is not defined
    
    Change-Id: I548053309079e66d2b644b1d21128d1079e8ba2f
    Signed-off-by: Vamsi Krishna <vskrishn@quicinc.com>

--- arch/arm/mach-msm/include/mach/rpc_hsusb.h
+++ arch/arm/mach-msm/include/mach/rpc_hsusb.h
@@ -25,6 +25,7 @@
 #include <mach/msm_otg.h>
 #include <mach/msm_hsusb.h>
 
+#ifdef CONFIG_MSM_SMD
 int msm_hsusb_rpc_connect(void);
 int msm_hsusb_phy_reset(void);
 int msm_hsusb_vbus_powerup(void);
@@ -60,4 +61,41 @@ int msm_fsusb_rpc_close(void);
 int msm_fsusb_remote_dev_disconnected(void);
 int msm_fsusb_set_remote_wakeup(void);
 void msm_fsusb_rpc_deinit(void);
+#else
+static inline int msm_hsusb_rpc_connect(void) { return 0; }
+static inline int msm_hsusb_phy_reset(void) { return 0; }
+static inline int msm_hsusb_vbus_powerup(void) { return 0; }
+static inline int msm_hsusb_vbus_shutdown(void) { return 0; }
+static inline int msm_hsusb_send_productID(uint32_t product_id) { return 0; }
+static inline int msm_hsusb_send_serial_number(char *serial_number)
+{ return 0; }
+static inline int msm_hsusb_is_serial_num_null(uint32_t val) { return 0; }
+static inline int msm_hsusb_reset_rework_installed(void) { return 0; }
+static inline int msm_hsusb_enable_pmic_ulpidata0(void) { return 0; }
+static inline int msm_hsusb_disable_pmic_ulpidata0(void) { return 0; }
+static inline int msm_hsusb_rpc_close(void) { return 0; }
+
+static inline int msm_chg_rpc_connect(void) { return 0; }
+static inline int msm_chg_usb_charger_connected(uint32_t type) { return 0; }
+static inline int msm_chg_usb_i_is_available(uint32_t sample) { return 0; }
+static inline int msm_chg_usb_i_is_not_available(void) { return 0; }
+static inline int msm_chg_usb_charger_disconnected(void) { return 0; }
+static inline int msm_chg_rpc_close(void) { return 0; }
+
+#ifdef CONFIG_USB_GADGET_MSM_72K
+static inline int hsusb_chg_init(int connect) { return 0; }
+static inline void hsusb_chg_vbus_draw(unsigned mA) { }
+static inline void hsusb_chg_connected(enum chg_type chgtype) { }
+#endif
+
+static inline int msm_fsusb_rpc_init(struct msm_otg_ops *ops) { return 0; }
+static inline int msm_fsusb_init_phy(void) { return 0; }
+static inline int msm_fsusb_reset_phy(void) { return 0; }
+static inline int msm_fsusb_suspend_phy(void) { return 0; }
+static inline int msm_fsusb_resume_phy(void) { return 0; }
+static inline int msm_fsusb_rpc_close(void) { return 0; }
+static inline int msm_fsusb_remote_dev_disconnected(void) { return 0; }
+static inline int msm_fsusb_set_remote_wakeup(void) { return 0; }
+static inline void msm_fsusb_rpc_deinit(void) { }
+#endif
 #endif
