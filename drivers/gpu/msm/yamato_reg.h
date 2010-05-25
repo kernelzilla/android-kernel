d19d5db81879909329bc6f55af0349fb7f02b822
Shubhraprakash Das <sadas@codeaurora.org>
2010-05-25 15:40:23 -0600

    msm_kgsl: Workaround for rbbm read errors and spurious interrupts
    Sometimes read errors are generated from rbbm while trying to read the
    CP interrupt status register. Also, there is a delay between
    acknowledging CP interrupts and the master interrupt line going down
    which causes spurious interrupts to be generated.
    
    Hence, we loop a finite number of times to successfully read the
    interrupt status register to workaround the above mentioned problems.
    
    Change-Id: Iff8f8c71777ad4ada32bef6bb15f4e0d00964638
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/yamato_reg.h
+++ drivers/gpu/msm/yamato_reg.h
@@ -151,6 +151,25 @@ union reg_rb_edram_info {
 	struct rb_edram_info_t f;
 };
 
+#define RBBM_READ_ERROR_UNUSED0_SIZE		2
+#define RBBM_READ_ERROR_READ_ADDRESS_SIZE	15
+#define RBBM_READ_ERROR_UNUSED1_SIZE		13
+#define RBBM_READ_ERROR_READ_REQUESTER_SIZE	1
+#define RBBM_READ_ERROR_READ_ERROR_SIZE		1
+
+struct rbbm_read_error_t {
+	unsigned int unused0:RBBM_READ_ERROR_UNUSED0_SIZE;
+	unsigned int read_address:RBBM_READ_ERROR_READ_ADDRESS_SIZE;
+	unsigned int unused1:RBBM_READ_ERROR_UNUSED1_SIZE;
+	unsigned int read_requester:RBBM_READ_ERROR_READ_REQUESTER_SIZE;
+	unsigned int read_error:RBBM_READ_ERROR_READ_ERROR_SIZE;
+};
+
+union rbbm_read_error_u {
+	unsigned int val:32;
+	struct rbbm_read_error_t f;
+};
+
 #define CP_RB_CNTL_RB_BUFSZ_SIZE                           6
 #define CP_RB_CNTL_UNUSED0_SIZE                            2
 #define CP_RB_CNTL_RB_BLKSZ_SIZE                           6
