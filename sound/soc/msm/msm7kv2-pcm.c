838d0b2a479cb8408a5459fcc972254f4066c20b
Asish Bhattacharya <asishb@qualcomm.com>
2010-05-28 18:40:05 +0530

    msm: alsa: 7x30: Fix the wrong check in trigger routine.
    Problem
    The driver checks wrong flag in trigger routine. This results in
    wrong state if the interrupt comes from dsp before the trigger start.
    The issue is reproducible if we play audio via alsa utils over serial
    console.
    
    Impact
    The driver does not play audio if there is lot of delay in user space
    to dump data from user space.
    
    Solution
    Correct check in driver shall resolve the issue as it wouldn't check the
    outneed flag if the playback is in non mmap interface.
    
    Change-Id: I6da4368dea1e729e291fa0aacaaeb66df7474874
    Signed-off-by: Asish Bhattacharya <asishb@qualcomm.com>

--- sound/soc/msm/msm7kv2-pcm.c
+++ sound/soc/msm/msm7kv2-pcm.c
@@ -278,7 +278,7 @@ static int msm_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
 	case SNDRV_PCM_TRIGGER_RESUME:
 	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
 		if ((substream->stream == SNDRV_PCM_STREAM_CAPTURE)
-			|| prtd->mmap_flag)
+			|| !prtd->mmap_flag)
 			break;
 		if (!prtd->out_needed) {
 			prtd->stopped = 0;
@@ -310,7 +310,8 @@ static int msm_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
 	case SNDRV_PCM_TRIGGER_STOP:
 	case SNDRV_PCM_TRIGGER_SUSPEND:
 	case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
-		if (substream->stream == SNDRV_PCM_STREAM_CAPTURE)
+		if ((substream->stream == SNDRV_PCM_STREAM_CAPTURE)
+			|| !prtd->mmap_flag)
 			break;
 		prtd->stopped = 1;
 		break;
