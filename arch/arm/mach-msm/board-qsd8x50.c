e9bf0562e9fcb60c1d5c2fb5f8e9b1da362493bb
Lena Salman <esalman@qualcomm.com>
2010-06-06 12:14:02 +0300

    msm: 8x50: Add spi clock name to board file
    Support of both QUP and QSD cores in the spi_qsd driver requires
    the board file to implicitly name the clock names in use.
    
    Change-Id: I63eb979f7f4aab0e97c306b96a18af829d02ad2e
    Signed-off-by: Lena Salman <esalman@qualcomm.com>

--- arch/arm/mach-msm/board-qsd8x50.c
+++ arch/arm/mach-msm/board-qsd8x50.c
@@ -734,6 +734,7 @@ static void msm_qsd_spi_gpio_release(void)
 
 static struct msm_spi_platform_data qsd_spi_pdata = {
 	.max_clock_speed = 19200000,
+	.clk_name = "spi_clk",
 	.gpio_config  = msm_qsd_spi_gpio_config,
 	.gpio_release = msm_qsd_spi_gpio_release,
 	.dma_config = msm_qsd_spi_dma_config,
