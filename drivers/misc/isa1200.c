7cead8023981c18d56bd14c1a4cdd5a398d5d4fb
Mohan Pallaka <mpallaka@codeaurora.org>
2010-07-06 20:17:09 +0530

    misc: isa1200: Update platform structure for isa1200
    ISA1200 can be operated in multiple configurations. Provide
    configuration options for host to give best values for its
    architecutre.
    
    Change-Id: I677be3e1d713cc0d1a06a960f4ac402aa2676bbe
    Signed-off-by: Mohan Pallaka <mpallaka@codeaurora.org>

--- drivers/misc/isa1200.c
+++ drivers/misc/isa1200.c
@@ -21,93 +21,15 @@
 #include <linux/i2c/isa1200.h>
 #include "../staging/android/timed_output.h"
 
-#define ISA1200_SCTRL0		0x00
-#define ISA1200_THSWRST		(1 << 7)
-#define ISA1200_EXT2DIV		(1 << 4)
-#define ISA1200_LDOADJ_24V	(0x9 << 0)
-#define ISA1200_LDOADJ_25V	(0xa << 0)
-#define ISA1200_LDOADJ_26V	(0xb << 0)
-#define ISA1200_LDOADJ_27V	(0xc << 0)
-#define ISA1200_LDOADJ_28V	(0xd << 0)
-#define ISA1200_LDOADJ_29V	(0xe << 0)
-#define ISA1200_LDOADJ_30V	(0xf << 0)
-#define ISA1200_LDOADJ_31V	(0x0 << 0)
-#define ISA1200_LDOADJ_32V	(0x1 << 0)
-#define ISA1200_LDOADJ_33V	(0x2 << 0)
-#define ISA1200_LDOADJ_34V	(0x3 << 0)
-#define ISA1200_LDOADJ_35V	(0x4 << 0)
-#define ISA1200_LDOADJ_36V	(0x5 << 0)
-#define ISA1200_LDOADJ_MASK	(0xf << 0)
 #define ISA1200_HCTRL0		0x30
-#define ISA1200_HAPDREN		(1 << 7)
-#define ISA1200_OVEREN		(1 << 6)
-#define ISA1200_OVERHL		(1 << 5)
-#define ISA1200_HAPDIGMOD_PWM_IN	(1 << 3)
-#define ISA1200_HAPDIGMOD_PWM_GEN	(2 << 3)
-#define ISA1200_PLLMOD		(1 << 2)
-#define ISA1200_PWMMOD_DIVIDER_128	(0 << 0)
-#define ISA1200_PWMMOD_DIVIDER_256	(1 << 0)
-#define ISA1200_PWMMOD_DIVIDER_512	(2 << 0)
-#define ISA1200_PWMMOD_DIVIDER_1024	(3 << 0)
 #define ISA1200_HCTRL1		0x31
-#define ISA1200_EXTCLKSEL	(1 << 7)
-#define ISA1200_BIT6_ON		(1 << 6)
-#define ISA1200_MOTTYP_ERM	(1 << 5)
-#define ISA1200_MOTTYP_LRA	(0 << 5)
-#define ISA1200_PLLEN		(1 << 4)
-#define ISA1200_SMARTEN		(1 << 3)
-#define ISA1200_SMARTONT	(1 << 2)
-#define ISA1200_SMARTOFFT_16	(0 << 0)
-#define ISA1200_SMARTOFFT_32	(1 << 0)
-#define ISA1200_SMARTOFFT_64	(2 << 0)
-#define ISA1200_SMARTOFFT_100	(3 << 0)
-#define ISA1200_HCTRL2		0x32
-#define ISA1200_HSWRST		(1 << 7)
-#define ISA1200_SESTMOD		(1 << 2)
-#define ISA1200_SEEN		(1 << 1)
-#define ISA1200_SEEVENT		(1 << 0)
-#define ISA1200_HCTRL3		0x33
-#define ISA1200_PPLLDIV_MASK	(0xf0)
-#define ISA1200_PPLLDIV_SHIFT	(4)
-#define ISA1200_PPLLDIV_1	(0x0)
-#define ISA1200_PPLLDIV_2	(0x1)
-#define ISA1200_PPLLDIV_4	(0x2)
-#define ISA1200_PPLLDIV_8	(0x3)
-#define ISA1200_PPLLDIV_16	(0x4)
-#define ISA1200_PPLLDIV_32	(0x5)
-#define ISA1200_PPLLDIV_64	(0x6)
-#define ISA1200_PPLLDIV_128	(0x7)
-#define ISA1200_WPLLDIV_MASK	(0x0f)
-#define ISA1200_WPLLDIV_SHIFT	(0)
-#define ISA1200_WPLLDIV_1	(0x0)
-#define ISA1200_WPPLLDIV_2	(0x1)
-#define ISA1200_WPPLLDIV_4	(0x2)
-#define ISA1200_WPPLLDIV_8	(0x3)
-#define ISA1200_WPPLLDIV_16	(0x4)
-#define ISA1200_WPPLLDIV_32	(0x5)
-#define ISA1200_WPPLLDIV_64	(0x6)
-#define ISA1200_WPPLLDIV_128	(0x7)
-#define ISA1200_HCTRL4		0x34
 #define ISA1200_HCTRL5		0x35
-#define ISA1200_HCTRL6		0x36
-#define ISA1200_HCTRL7		0x37
-#define ISA1200_HCTRL8		0x38
-#define ISA1200_HCTRL9		0x39
-#define ISA1200_PLLP_SHIFT	(4)
-#define ISA1200_PLLP_MASK	(0xf0)
-#define ISA1200_PLLS_SHIFT	(0)
-#define ISA1200_PLLS_MASK	(0x0f)
-#define ISA1200_HCTRLA		0x3A
-#define ISA1200_PLLMM_MASK	(0xfc)
-#define ISA1200_PLLMM_SHIFT	(2)
-#define ISA1200_PLLMS_MASK	(0x03)
-#define ISA1200_PLLMS_SHIFT	(0)
-#define ISA1200_HCTRLB		0x3B
-#define ISA1200_HCTRLC		0x3C
-#define ISA1200_HCTRLD		0x3D
 
-#define PWM_HAPTIC_PERIOD	44640
-#define PWM_HAPTIC_DEFAULT_LEVEL	99
+#define ISA1200_HCTRL0_RESET	0x01
+#define ISA1200_HCTRL1_RESET	0x4B
+
+#define ISA1200_HCTRL5_VIB_STRT	0xD5
+#define ISA1200_HCTRL5_VIB_STOP	0x6B
 
 struct isa1200_chip {
 	struct i2c_client *client;
@@ -117,15 +39,9 @@ struct isa1200_chip {
 	struct timed_output_dev dev;
 	struct work_struct work;
 	spinlock_t lock;
-
-	int enable;
-	int powered;
+	unsigned int enable;
+	unsigned int period_ns;
 };
-
-static int isa1200_chip_set_pwm_cycle(struct isa1200_chip *haptic)
-{
-	return pwm_config(haptic->pwm, PWM_HAPTIC_PERIOD/2, PWM_HAPTIC_PERIOD);
-}
 
 static int isa1200_read_reg(struct i2c_client *client, int reg)
 {
@@ -149,40 +65,47 @@ static int isa1200_write_reg(struct i2c_client *client, int reg, u8 value)
 	return ret;
 }
 
-static void isa1200_chip_power_on(struct isa1200_chip *haptic)
+static void isa1200_vib_set(struct isa1200_chip *haptic, int enable)
 {
-	if (haptic->powered)
-		return;
-	haptic->powered = 1;
-	/* Use smart mode enable control */
-	pwm_enable(haptic->pwm);
-}
+	int rc = 0;
 
-static void isa1200_chip_power_off(struct isa1200_chip *haptic)
-{
-	if (!haptic->powered)
-		return;
-	haptic->powered = 0;
-	/* Use smart mode enable control */
-	pwm_disable(haptic->pwm);
+	if (enable) {
+		if (haptic->pdata->mode_ctrl == PWM_INPUT_MODE) {
+			int period_us = haptic->period_ns / 1000;
+			rc = pwm_config(haptic->pwm,
+				(period_us * haptic->pdata->duty) / 100,
+				period_us);
+			if (rc < 0)
+				pr_err("%s: pwm_config fail\n", __func__);
+			rc = pwm_enable(haptic->pwm);
+			if (rc < 0)
+				pr_err("%s: pwm_enable fail\n", __func__);
+		} else if (haptic->pdata->mode_ctrl == PWM_GEN_MODE) {
+			rc = isa1200_write_reg(haptic->client,
+						ISA1200_HCTRL5,
+						ISA1200_HCTRL5_VIB_STRT);
+			if (rc < 0)
+				pr_err("%s: start vibartion fail\n", __func__);
+		}
+	} else {
+		if (haptic->pdata->mode_ctrl == PWM_INPUT_MODE)
+			pwm_disable(haptic->pwm);
+		else if (haptic->pdata->mode_ctrl == PWM_GEN_MODE) {
+			rc = isa1200_write_reg(haptic->client,
+						ISA1200_HCTRL5,
+						ISA1200_HCTRL5_VIB_STOP);
+			if (rc < 0)
+				pr_err("%s: stop vibartion fail\n", __func__);
+		}
+	}
 }
 
 static void isa1200_chip_work(struct work_struct *work)
 {
 	struct isa1200_chip *haptic;
-	int ret;
 
 	haptic = container_of(work, struct isa1200_chip, work);
-	if (haptic->enable) {
-		ret = isa1200_chip_set_pwm_cycle(haptic);
-		if (ret) {
-			dev_dbg(haptic->dev.dev, "set_pwm_cycle failed\n");
-			return;
-		}
-		isa1200_chip_power_on(haptic);
-	} else {
-		isa1200_chip_power_off(haptic);
-	}
+	isa1200_vib_set(haptic, haptic->enable);
 }
 
 static void isa1200_chip_enable(struct timed_output_dev *dev, int value)
@@ -225,28 +148,72 @@ static enum hrtimer_restart isa1200_vib_timer_func(struct hrtimer *timer)
 					timer);
 	haptic->enable = 0;
 	schedule_work(&haptic->work);
+
 	return HRTIMER_NORESTART;
 }
-static void isa1200_setup(struct i2c_client *client)
+
+static void dump_isa1200_reg(char *str, struct i2c_client *client)
+{
+	pr_debug("%s reg0x%x=0x%x, reg0x%x=0x%x, reg0x%x=0x%x\n", str,
+		ISA1200_HCTRL0, isa1200_read_reg(client, ISA1200_HCTRL0),
+		ISA1200_HCTRL1, isa1200_read_reg(client, ISA1200_HCTRL1),
+		ISA1200_HCTRL5, isa1200_read_reg(client, ISA1200_HCTRL5));
+}
+
+static int isa1200_setup(struct i2c_client *client)
 {
 	struct isa1200_chip *haptic = i2c_get_clientdata(client);
-	int value;
+	int value, temp, rc;
 
 	gpio_set_value(haptic->pdata->hap_en_gpio, 0);
 	udelay(250);
 	gpio_set_value(haptic->pdata->hap_en_gpio, 1);
 
-	value = ISA1200_HAPDREN | ISA1200_OVERHL | ISA1200_HAPDIGMOD_PWM_IN |
-		ISA1200_PWMMOD_DIVIDER_128;
-	isa1200_write_reg(client, ISA1200_HCTRL0, value);
+	value =	(haptic->pdata->smart_en << 3) |
+		(haptic->pdata->is_erm << 5) |
+		(haptic->pdata->ext_clk_en << 7);
 
-	value = ISA1200_EXTCLKSEL | ISA1200_BIT6_ON | ISA1200_MOTTYP_LRA |
-		ISA1200_SMARTEN | ISA1200_SMARTOFFT_64;
-	isa1200_write_reg(client, ISA1200_HCTRL1, value);
+	rc = isa1200_write_reg(client, ISA1200_HCTRL1, value);
+	if (rc < 0) {
+		pr_err("%s: i2c write failure\n", __func__);
+		return rc;
+	}
 
-	value = isa1200_read_reg(client, ISA1200_HCTRL2);
-	value |= ISA1200_SEEN;
-	isa1200_write_reg(client, ISA1200_HCTRL2, value);
+	if (haptic->pdata->mode_ctrl == PWM_GEN_MODE) {
+		temp = haptic->pdata->pwm_fd.pwm_div;
+		if (temp < 128 || temp > 1024 || temp % 128) {
+			pr_err("%s: Invalid divider\n", __func__);
+			goto reset_hctrl1;
+		}
+		value = ((temp >> 7) - 1);
+	} else if (haptic->pdata->mode_ctrl == PWM_INPUT_MODE) {
+		temp = haptic->pdata->pwm_fd.pwm_freq;
+		if (temp < 22400 || temp > 172600 || temp % 22400) {
+			pr_err("%s: Invalid frequency\n", __func__);
+			goto reset_hctrl1;
+		}
+		value = ((temp / 22400) - 1);
+		haptic->period_ns = NSEC_PER_SEC / temp;
+	}
+
+	value |= (haptic->pdata->mode_ctrl << 3) |
+		(haptic->pdata->overdrive_high << 5) |
+		(haptic->pdata->overdrive_en << 5) |
+		(haptic->pdata->chip_en << 7);
+
+	rc = isa1200_write_reg(client, ISA1200_HCTRL0, value);
+	if (rc < 0) {
+		pr_err("%s: i2c write failure\n", __func__);
+		goto reset_hctrl1;
+	}
+
+	dump_isa1200_reg("new:", client);
+	return 0;
+
+reset_hctrl1:
+	i2c_smbus_write_byte_data(client, ISA1200_HCTRL1,
+				ISA1200_HCTRL1_RESET);
+	return rc;
 }
 
 static int __devinit isa1200_probe(struct i2c_client *client,
@@ -286,13 +253,6 @@ static int __devinit isa1200_probe(struct i2c_client *client,
 		}
 	}
 
-	haptic->pwm = pwm_request(pdata->pwm_ch_id, id->name);
-	if (IS_ERR(haptic->pwm)) {
-		dev_err(&client->dev, "%s: pwm request failed\n", __func__);
-		ret = PTR_ERR(haptic->pwm);
-		goto pwm_req_fail;
-	}
-
 	spin_lock_init(&haptic->lock);
 	INIT_WORK(&haptic->work, isa1200_chip_work);
 
@@ -323,15 +283,31 @@ static int __devinit isa1200_probe(struct i2c_client *client,
 		goto gpio_fail;
 	}
 
-	isa1200_setup(client);
+	ret = isa1200_setup(client);
+	if (ret) {
+		dev_err(&client->dev, "%s: setup fail %d\n", __func__, ret);
+		goto gpio_fail;
+	}
+
+	if (haptic->pdata->mode_ctrl == PWM_INPUT_MODE) {
+		haptic->pwm = pwm_request(pdata->pwm_ch_id, id->name);
+		if (IS_ERR(haptic->pwm)) {
+			dev_err(&client->dev, "%s: pwm request failed\n",
+							__func__);
+			ret = PTR_ERR(haptic->pwm);
+			goto reset_hctrl0;
+		}
+	}
+
 	printk(KERN_INFO "%s: %s registered\n", __func__, id->name);
 	return 0;
 
+reset_hctrl0:
+	i2c_smbus_write_byte_data(client, ISA1200_HCTRL0,
+					ISA1200_HCTRL0_RESET);
 gpio_fail:
 	timed_output_dev_unregister(&haptic->dev);
 timed_reg_fail:
-	pwm_free(haptic->pwm);
-pwm_req_fail:
 	if (pdata->power_on)
 		pdata->power_on(0);
 pwr_up_fail:
@@ -343,34 +319,54 @@ static int __devexit isa1200_remove(struct i2c_client *client)
 {
 	struct isa1200_chip *haptic = i2c_get_clientdata(client);
 
-	cancel_work_sync(&haptic->work);
 	hrtimer_cancel(&haptic->timer);
+	cancel_work_sync(&haptic->work);
+
+	/* turn-off current vibration */
+	isa1200_vib_set(haptic, 0);
+
+	if (haptic->pdata->mode_ctrl == PWM_INPUT_MODE)
+		pwm_free(haptic->pwm);
+
 	timed_output_dev_unregister(&haptic->dev);
 	gpio_free(haptic->pdata->hap_en_gpio);
 
+	/* reset hardware registers */
+	i2c_smbus_write_byte_data(client, ISA1200_HCTRL0,
+				ISA1200_HCTRL0_RESET);
+	i2c_smbus_write_byte_data(client, ISA1200_HCTRL1,
+				ISA1200_HCTRL1_RESET);
+
+	/* power-off the chip */
 	if (haptic->pdata->power_on)
 		haptic->pdata->power_on(0);
 
-	pwm_free(haptic->pwm);
 	kfree(haptic);
 	return 0;
 }
+
 #ifdef CONFIG_PM
 static int isa1200_suspend(struct i2c_client *client, pm_message_t mesg)
 {
 	struct isa1200_chip *haptic = i2c_get_clientdata(client);
+	int ret;
 
-	cancel_work_sync(&haptic->work);
 	hrtimer_cancel(&haptic->timer);
-	isa1200_chip_power_off(haptic);
+	cancel_work_sync(&haptic->work);
+	/* turn-off current vibration */
+	isa1200_vib_set(haptic, 0);
 
-	if (haptic->pdata->power_on)
-		haptic->pdata->power_on(0);
+	if (haptic->pdata->power_on) {
+		ret = haptic->pdata->power_on(0);
+		if (ret) {
+			dev_err(&client->dev, "power-down failed\n");
+			return ret;
+		}
+	}
 
 	return 0;
 }
 
-/* REVISIT: resolve i2c errors while resume though motors work fine*/
 static int isa1200_resume(struct i2c_client *client)
 {
 	struct isa1200_chip *haptic = i2c_get_clientdata(client);
--- include/linux/i2c/isa1200.h
+++ include/linux/i2c/isa1200.h
@@ -13,11 +13,32 @@
 #ifndef __LINUX_ISA1200_H
 #define __LINUX_ISA1200_H
 
+enum mode_control {
+	POWER_DOWN_MODE = 0,
+	PWM_INPUT_MODE,
+	PWM_GEN_MODE,
+	WAVE_GEN_MODE
+};
+
+union pwm_div_freq {
+	unsigned int pwm_div; /* PWM gen mode */
+	unsigned int pwm_freq; /* PWM input mode */
+};
+
 struct isa1200_platform_data {
-	char name[30];
-	int pwm_ch_id; /* pwm channel id */
-	int hap_en_gpio; /* haptic enable gpio */
-	int max_timeout;
+	const char *name;
+	unsigned int pwm_ch_id; /* pwm channel id */
+	unsigned int max_timeout;
+	unsigned int hap_en_gpio;
+	bool overdrive_high; /* high/low overdrive */
+	bool overdrive_en; /* enable/disable overdrive */
+	enum mode_control mode_ctrl; /* input/generation/wave */
+	union pwm_div_freq pwm_fd;
+	bool smart_en; /* smart mode enable/disable */
+	bool is_erm;
+	bool ext_clk_en;
+	unsigned int chip_en;
+	unsigned int duty;
 	int (*power_on)(int on);
 };
 
