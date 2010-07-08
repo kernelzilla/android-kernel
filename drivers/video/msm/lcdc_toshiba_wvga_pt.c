781c6b0fbe05c56bda8d15359907d9e583231ee7
Rajesh Sastrula <vrajesh@codeaurora.org>
2010-07-08 10:22:03 -0700

    msm_fb: display: remove spi bit-banging in lcdc
    lcdc panel drivers are using spi bit-banging and causing a
    GPIO conflit with MSM SPI controller driver.Hence remove
    the bit-banging and using the MSM SPI controller
    for SPI transfer.
    
    Change-Id: I37abe848ed5f486ba0a8709512a076421fc58299
    CRs-fixed: 220233
    Signed-off-by: Rajesh Sastrula <vrajesh@codeaurora.org>

--- drivers/video/msm/lcdc_toshiba_wvga_pt.c
+++ drivers/video/msm/lcdc_toshiba_wvga_pt.c
@@ -18,6 +18,9 @@
 
 #include <linux/delay.h>
 #include <linux/module.h>
+#ifdef CONFIG_SPI_QSD
+#include <linux/spi/spi.h>
+#endif
 #include <mach/gpio.h>
 #include <mach/pmic.h>
 #include "msm_fb.h"
@@ -26,11 +29,15 @@
 #include "mddihosti.h"
 #endif
 
+#ifdef CONFIG_SPI_QSD
+#define LCDC_TOSHIBA_SPI_DEVICE_NAME "lcdc_toshiba_ltm030dd40"
+static struct spi_device *lcdc_toshiba_spi_client;
+#else
 static int spi_cs;
 static int spi_sclk;
 static int spi_mosi;
 static int spi_miso;
-
+#endif
 struct toshiba_state_type{
 	boolean disp_initialized;
 	boolean display_on;
@@ -40,6 +47,7 @@ struct toshiba_state_type{
 static struct toshiba_state_type toshiba_state = { 0 };
 static struct msm_panel_common_pdata *lcdc_toshiba_pdata;
 
+#ifndef CONFIG_SPI_QSD
 static void toshiba_spi_write_byte(char dc, uint8 data)
 {
 	uint32 bit;
@@ -66,11 +74,58 @@ static void toshiba_spi_write_byte(char dc, uint8 data)
 		bnum--;
 	}
 }
+#endif
 
-static void toshiba_spi_write(char cmd, uint32 data, int num)
+static int toshiba_spi_write(char cmd, uint32 data, int num)
 {
 	char *bp;
+#ifdef CONFIG_SPI_QSD
+	char                tx_buf[4];
+	int                 rc, i;
+	struct spi_message  m;
+	struct spi_transfer t;
+	uint32 final_data = 0;
 
+	if (!lcdc_toshiba_spi_client) {
+		printk(KERN_ERR "%s lcdc_toshiba_spi_client is NULL\n",
+			__func__);
+		return -EINVAL;
+	}
+
+	memset(&t, 0, sizeof t);
+	t.tx_buf = tx_buf;
+	spi_setup(lcdc_toshiba_spi_client);
+	spi_message_init(&m);
+	spi_message_add_tail(&t, &m);
+
+	/* command byte first */
+	final_data |= cmd << 23;
+	t.len = num + 2;
+	if (t.len < 4)
+		t.bits_per_word = 8 * t.len;
+	/* followed by parameter bytes */
+	if (num) {
+		bp = (char *)&data;;
+		bp += (num - 1);
+		i = 1;
+		while (num) {
+			final_data |= 1 << (((4 - i) << 3) - i - 1);
+			final_data |= *bp << (((4 - i - 1) << 3) - i - 1);
+			num--;
+			bp--;
+			i++;
+		}
+	}
+
+	bp = (char *)&final_data;
+	for (i = 0; i < t.len; i++)
+		tx_buf[i] = bp[3 - i];
+	t.rx_buf = NULL;
+	rc = spi_sync(lcdc_toshiba_spi_client, &m);
+	if (rc)
+		printk(KERN_ERR "spi_sync _write failed %d\n", rc);
+	return rc;
+#else
 	gpio_set_value(spi_cs, 1);	/* cs high */
 
 	/* command byte first */
@@ -89,10 +144,49 @@ static void toshiba_spi_write(char cmd, uint32 data, int num)
 
 	gpio_set_value(spi_cs, 0);	/* cs low */
 	udelay(1);
+	return 0;
+#endif
 }
 
-void toshiba_spi_read_bytes(char cmd, uint32 *data, int num)
+static int toshiba_spi_read_bytes(char cmd, uint32 *data, int num)
 {
+#ifdef CONFIG_SPI_QSD
+	char            tx_buf[5];
+	char		    rx_buf[5];
+	int                 rc;
+	struct spi_message  m;
+	struct spi_transfer t;
+
+	if (!lcdc_toshiba_spi_client) {
+		printk(KERN_ERR "%s lcdc_toshiba_spi_client is NULL\n",
+			 __func__);
+		return -EINVAL;
+	}
+
+	memset(&t, 0, sizeof t);
+	t.tx_buf = tx_buf;
+	t.rx_buf = rx_buf;
+	spi_setup(lcdc_toshiba_spi_client);
+	spi_message_init(&m);
+	spi_message_add_tail(&t, &m);
+
+	/* command byte first */
+	tx_buf[0] = 0 | ((cmd >> 1) & 0x7f);
+	tx_buf[1] = (cmd & 0x01) << 7;
+	tx_buf[2] = 0;
+	tx_buf[3] = 0;
+	tx_buf[4] = 0;
+
+	t.len = 5;
+
+	rc = spi_sync(lcdc_toshiba_spi_client, &m);
+	*data = 0;
+	*data = ((rx_buf[1] & 0x1f) << 19) | (rx_buf[2] << 11) |
+		(rx_buf[3] << 3) | ((rx_buf[4] & 0xe0) >> 5);
+	if (rc)
+		printk(KERN_ERR "spi_sync _read failed %d\n", rc);
+	return rc;
+#else
 	uint32 dbit, bits;
 	int bnum;
 
@@ -129,8 +223,11 @@ void toshiba_spi_read_bytes(char cmd, uint32 *data, int num)
 	udelay(1);
 	gpio_set_value(spi_cs, 0);	/* cs low */
 	udelay(1);
+	return 0;
+#endif
 }
 
+#ifndef CONFIG_SPI_QSD
 static void spi_pin_assign(void)
 {
 	/* Setting the Default GPIO's */
@@ -139,6 +236,7 @@ static void spi_pin_assign(void)
 	spi_mosi  = *(lcdc_toshiba_pdata->gpio_num + 2);
 	spi_miso  = *(lcdc_toshiba_pdata->gpio_num + 3);
 }
+#endif
 
 static void toshiba_disp_powerup(void)
 {
@@ -153,10 +251,12 @@ static void toshiba_disp_on(void)
 {
 	uint32	data;
 
+#ifndef CONFIG_SPI_QSD
 	gpio_set_value(spi_cs, 0);	/* low */
 	gpio_set_value(spi_sclk, 1);	/* high */
 	gpio_set_value(spi_mosi, 0);
 	gpio_set_value(spi_miso, 0);
+#endif
 
 	if (toshiba_state.disp_powered_up && !toshiba_state.display_on) {
 		toshiba_spi_write(0, 0, 0);
@@ -302,13 +402,37 @@ static int __init toshiba_probe(struct platform_device *pdev)
 {
 	if (pdev->id == 0) {
 		lcdc_toshiba_pdata = pdev->dev.platform_data;
+#ifndef CONFIG_SPI_QSD
 		spi_pin_assign();
+#endif
 		return 0;
 	}
 	msm_fb_add_device(pdev);
 	return 0;
 }
 
+#ifdef CONFIG_SPI_QSD
+static int __devinit lcdc_toshiba_spi_probe(struct spi_device *spi)
+{
+	lcdc_toshiba_spi_client = spi;
+	lcdc_toshiba_spi_client->bits_per_word = 32;
+	return 0;
+}
+static int __devexit lcdc_toshiba_spi_remove(struct spi_device *spi)
+{
+	lcdc_toshiba_spi_client = NULL;
+	return 0;
+}
+
+static struct spi_driver lcdc_toshiba_spi_driver = {
+	.driver = {
+		.name  = LCDC_TOSHIBA_SPI_DEVICE_NAME,
+		.owner = THIS_MODULE,
+	},
+	.probe         = lcdc_toshiba_spi_probe,
+	.remove        = __devexit_p(lcdc_toshiba_spi_remove),
+};
+#endif
 static struct platform_driver this_driver = {
 	.probe  = toshiba_probe,
 	.driver = {
@@ -372,9 +496,27 @@ static int __init lcdc_toshiba_panel_init(void)
 	pinfo->lcdc.hsync_skew = 0;
 
 	ret = platform_device_register(&this_device);
-	if (ret)
-		platform_driver_unregister(&this_driver);
+	if (ret) {
+		printk(KERN_ERR "%s not able to register the device\n",
+			 __func__);
+		goto fail_driver;
+	}
+#ifdef CONFIG_SPI_QSD
+	ret = spi_register_driver(&lcdc_toshiba_spi_driver);
 
+	if (ret) {
+		printk(KERN_ERR "%s not able to register spi\n", __func__);
+		goto fail_device;
+	}
+#endif
+	return ret;
+
+#ifdef CONFIG_SPI_QSD
+fail_device:
+	platform_device_unregister(&this_device);
+#endif
+fail_driver:
+	platform_driver_unregister(&this_driver);
 	return ret;
 }
 
