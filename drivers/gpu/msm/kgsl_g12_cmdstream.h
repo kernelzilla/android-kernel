5092ccf8f7e14790048a1f76b8a044e4b272afdd
Jeremy Gebben <jgebben@codeaurora.org>
2010-07-19 09:54:11 -0600

    msm: kgsl: restructure 2d ringbuffer setup
    The ringbuffer should be setup during initialization, not
    when the first context is created.
    
    Change-Id: I756164b90eed79b7f88fefc102afd0a4b4d31d52
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- drivers/gpu/msm/kgsl_g12_cmdstream.h
+++ drivers/gpu/msm/kgsl_g12_cmdstream.h
@@ -39,6 +39,12 @@
 
 struct kgsl_g12_device;
 
+int kgsl_g12_cmdstream_init(struct kgsl_device *device);
+
+int kgsl_g12_cmdstream_start(struct kgsl_device *device);
+
+void kgsl_g12_cmdstream_close(struct kgsl_device *device);
+
 int kgsl_g12_cmdstream_check_timestamp(struct kgsl_g12_device *g12_device,
 					unsigned int timestamp);
 unsigned int kgsl_g12_cmdstream_readtimestamp(struct kgsl_device *device,
