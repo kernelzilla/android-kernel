893efcc8a447ff96f495b12cbec678fe11ed147e
Ashraya Arya <ashrayaa@codeaurora.org>
2010-07-22 18:58:27 -0700

    qsd8k: audio: Add RX  mute functionality to the audio driver
    Volume control API functionality expanded to interpret input param of
    0 as RX mute.
    
    Change-Id: I25bb2930c46a56719cd5ae6acd390b8c22ea43d8
    Signed-off-by: Ashraya Arya <ashrayaa@codeaurora.org>

--- arch/arm/mach-msm/qdsp6/q6audio.c
+++ arch/arm/mach-msm/qdsp6/q6audio.c
@@ -1373,9 +1373,14 @@ int q6audio_set_rx_volume(int level)
 
 	mutex_lock(&audio_path_lock);
 	adev = ADSP_AUDIO_DEVICE_ID_VOICE;
-	vol = q6_device_volume(audio_rx_device_id, level);
-	audio_rx_mute(ac_control, adev, 0);
-	audio_rx_volume(ac_control, adev, vol);
+
+	if (level) {
+		vol = q6_device_volume(audio_rx_device_id, level);
+		audio_rx_mute(ac_control, adev, 0);
+		audio_rx_volume(ac_control, adev, vol);
+	} else
+		audio_rx_mute(ac_control, adev, 1);
+
 	rx_vol_level = level;
 	mutex_unlock(&audio_path_lock);
 	return 0;
