5f46d1a94b75aa3541cd6ca010d0d44f75b2fdf7
Steve Kondik <shade@chemlab.org>
2010-07-28 11:08:49 -0400

    camera: Updated s5k3e2fx driver from HTC.

---
+++ drivers/media/video/msm/s5k3e2fx.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2008-2009 QUALCOMM Incorporated.
+ */
+
+#ifndef S5K3E2FX_H
+#define S5K3E2FX_H
+
+#include <mach/board.h>
+
+
+unsigned char g_usPowerDownFlag = false;
+unsigned char g_usLCSettingFlag = false;
+uint16_t g_usModuleVersion = 0; /*0: rev.4, 1: rev.5*/
+
+#endif /* CAMSENSOR_S5K3E2FX */
