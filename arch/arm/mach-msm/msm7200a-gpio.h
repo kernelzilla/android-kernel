edadfe8b74c6254c89616f41aea026a6ec23bfef
Gregory Bean <gbean@codeaurora.org>
2010-07-28 16:41:33 -0700

    msm: gpio: Backport of msm7200a from upstream.
    Now that msm7200a is accepted upstream, backport
    all changes resulting from upstream code review.
    
    Change-Id: I8ce6b090c72d1f80cbd57511d69d67e3a004a71c
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>

--- arch/arm/mach-msm/msm7200a-gpio.h
+++ arch/arm/mach-msm/msm7200a-gpio.h
@@ -29,6 +29,20 @@
 #ifndef __LINUX_MSM7200A_GPIO_H
 #define __LINUX_MSM7200A_GPIO_H
 
+/**
+ * struct msm7200a_gpio_regs - addresess of iomapped GPIO registers
+ * @in:		GPIO_IN_n
+ * @out:	GPIO_OUT_n
+ * @oe:		GPIO_OE_n
+ * @int_status:	GPIO_INT_STATUS_n
+ * @int_clear:	GPIO_INT_CLEAR_n
+ * @int_en:	GPIO_INT_EN_n
+ * @int_edge:	GPIO_INT_EDGE_n
+ * @int_pos:	GPIO_INT_POS_n
+ *
+ * Registers are not guaranteed to be packed in memory, or even
+ * located in a predictable pattern.
+ */
 struct msm7200a_gpio_regs {
 	void __iomem *in;
 	void __iomem *out;
@@ -40,11 +54,33 @@ struct msm7200a_gpio_regs {
 	void __iomem *int_pos;
 };
 
+/**
+ * struct msm7200a_gpio_platform_data - configuration for msm7200a-gpio
+ * @gpio_base:	The first gpio to be assigned to the device.  Corresponds
+ *		directly to gpio_chip.base.
+ * @ngpio:	The number of gpio lines to be managed by the device.
+ *		Must be <= 32.  Corresponds directly to gpio_chip.ngpio.
+ * @irq_base:	The first irq to be assigned to the device.  The gpio
+ *		at 'gpio_base' will be assigned irq 'irq_base',
+ *		gpio 'gpio_base + 1' will receive irq 'irq_base + 1',
+ *		and so on.
+ * @irq_summary:	The summary irq line which will be used by the device
+ *			to notify the kernel when an interrupt occurs on
+ *			any of its gpio lines.  Most MSM SoCs have more
+ *			than one gpio device sharing each of these.
+ * @latch_level_irqs:	The MSM gpio hardware latches level interrupts,
+ *			which is atypical.  Setting this flag to false
+ *			makes the driver compensate for this and produce
+ *			the traditional unlatched behavior for level irqs.
+ * @regs:	Addresses of the registers which control the gpios
+ *		to be managed by the device.
+ */
 struct msm7200a_gpio_platform_data {
 	unsigned gpio_base;
 	unsigned ngpio;
 	unsigned irq_base;
 	unsigned irq_summary;
+	bool latch_level_irqs;
 	struct msm7200a_gpio_regs regs;
 };
 
