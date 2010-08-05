4ad1560ef4f0fb38244cb34a3fc2f34b000a4998
Patrick Jacques <kernelzilla@kinetic-computing.com>
2010-08-04 23:57:16 -0700

    [ARM] msm_camera: add mutex lock to ioctl
    Add a mutex lock to fix concurrency issue of processing
    more than one blocking call at a time in MSM_CAM_IOCTL_CTRL_COMMAND.
    Also apply to trout/sapphire msm_camera_old driver.
    
    author: Nagesh Reddy <nageshsreddy@codeaurora.org>
    codeaurora commit: fd27fc8f1edd31ed64d5f8aa08aa2f1ec2420534

--- drivers/media/video/msm/msm_camera_old.c
+++ drivers/media/video/msm/msm_camera_old.c
@@ -39,6 +39,7 @@
 #include <linux/sched.h>
 
 #define MSM_MAX_CAMERA_SENSORS 5
+DEFINE_MUTEX(ctrl_cmd_lock);
 
 #define ERR_USER_COPY(to) pr_err("%s(%d): copy %s user\n", \
 				__func__, __LINE__, ((to) ? "to" : "from"))
@@ -1824,7 +1825,9 @@ static long msm_ioctl_control(struct file *filep, unsigned int cmd,
 	case MSM_CAM_IOCTL_CTRL_COMMAND:
 		/* Coming from control thread, may need to wait for
 		 * command status */
+		mutex_lock(&ctrl_cmd_lock);
 		rc = msm_control(ctrl_pmsm, 1, argp);
+		mutex_unlock(&ctrl_cmd_lock);
 		break;
 	case MSM_CAM_IOCTL_CTRL_COMMAND_2:
 		/* Sends a message, returns immediately */
