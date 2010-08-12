27dd11ae1817a7f383a5def98a4a98b71c79979f
Steve Kondik <shade@chemlab.org>
2010-08-11 21:54:43 -0400

    mahimahi: Rearrange memory, kang another 12MB

--- arch/arm/mach-msm/board-mahimahi.c
+++ arch/arm/mach-msm/board-mahimahi.c
@@ -1153,10 +1153,10 @@ static void __init mahimahi_fixup(struct machine_desc *desc, struct tag *tags,
 	mi->nr_banks = 2;
 	mi->bank[0].start = PHYS_OFFSET;
 	mi->bank[0].node = PHYS_TO_NID(PHYS_OFFSET);
-	mi->bank[0].size = (232*1024*1024);
-	mi->bank[1].start = MSM_HIGHMEM_BASE;
-	mi->bank[1].node = PHYS_TO_NID(MSM_HIGHMEM_BASE);
-	mi->bank[1].size = MSM_HIGHMEM_SIZE;
+	mi->bank[0].size = MSM_EBI1_BANK0_SIZE;
+	mi->bank[1].start = MSM_EBI1_BANK1_BASE;
+	mi->bank[1].node = PHYS_TO_NID(MSM_EBI1_BANK1_BASE);
+	mi->bank[1].size = MSM_EBI1_BANK1_SIZE;
 }
 
 static void __init mahimahi_map_io(void)
