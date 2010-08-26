2ed1591129e99699d28a00a79366e7d06a48df95
Gregory Bean <gbean@codeaurora.org>
2010-08-26 13:48:58 -0700

    msm: 8x60: Move basic tlmm configs to gpiomux.
    Begin the migration to gpiomux with some basic configurations:
    the powerdown-hold line, the GSBI lines, EBI2 headers,
    and the pins for the serial UART.
    
    Change-Id: Ia08c0968ebb12171395b782b646d0f30ade6bc88
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>

--- arch/arm/mach-msm/gpiomux-8x60.c
+++ arch/arm/mach-msm/gpiomux-8x60.c
@@ -20,6 +20,45 @@
 #include "tlmm-msm8660.h"
 #include "gpiomux.h"
 
+#define CONSOLE_UART	(GPIOMUX_FUNC_2 | GPIOMUX_DRV_8MA | GPIOMUX_VALID)
+
+#ifdef CONFIG_I2C_QUP
+#define GSBI3	(GPIOMUX_FUNC_1 | GPIOMUX_DRV_8MA | GPIOMUX_VALID)
+#ifdef CONFIG_MSM_CAMERA
+#define GSBI4	(GPIOMUX_FUNC_1 | GPIOMUX_DRV_8MA | GPIOMUX_VALID)
+#else
+#define GSBI4	0
+#endif
+#define GSBI7	(GPIOMUX_FUNC_1 | GPIOMUX_DRV_8MA | GPIOMUX_VALID)
+#define GSBI8	(GPIOMUX_FUNC_1 | GPIOMUX_VALID)
+#else
+#define GSBI3	0
+#define GSBI4	0
+#define GSBI7	0
+#define GSBI8	0
+#endif
+
+#if defined(CONFIG_SPI_QUP) || defined(CONFIG_SPI_QUP_MODULE)
+#define GSBI1	(GPIOMUX_FUNC_1 | GPIOMUX_DRV_8MA | GPIOMUX_VALID)
+#else
+#define GSBI1	0
+#endif
+
+#define PS_HOLD	(GPIOMUX_FUNC_1 | GPIOMUX_DRV_12MA | GPIOMUX_VALID)
+
+#define EBI2_A_D	(GPIOMUX_FUNC_1 | GPIOMUX_PULL_UP | GPIOMUX_DRV_8MA |\
+			 GPIOMUX_VALID)
+#define EBI2_OE		(GPIOMUX_FUNC_1 | GPIOMUX_PULL_UP | GPIOMUX_DRV_8MA |\
+			 GPIOMUX_VALID)
+#define EBI2_WE		(GPIOMUX_FUNC_1 | GPIOMUX_PULL_UP | GPIOMUX_DRV_8MA |\
+			 GPIOMUX_VALID)
+#define EBI2_CS2	(GPIOMUX_FUNC_2 | GPIOMUX_PULL_UP | GPIOMUX_DRV_8MA |\
+			 GPIOMUX_VALID)
+#define EBI2_CS3	(GPIOMUX_FUNC_1 | GPIOMUX_PULL_UP | GPIOMUX_DRV_8MA |\
+			 GPIOMUX_VALID)
+#define EBI2_ADV	(GPIOMUX_FUNC_1 | GPIOMUX_PULL_UP | GPIOMUX_DRV_8MA |\
+			 GPIOMUX_VALID)
+
 #ifdef CONFIG_MMC_MSM_SDC1_SUPPORT
 	#define SDCC1_DAT_0_3_CMD_ACTV_CFG (GPIOMUX_VALID | GPIOMUX_PULL_UP\
 					| GPIOMUX_FUNC_1 | GPIOMUX_DRV_10MA)
@@ -78,6 +117,144 @@
 #define SDCC5_SUSPEND_CONFIG (GPIOMUX_VALID | GPIOMUX_PULL_DOWN)
 
 struct msm_gpiomux_config msm_gpiomux_configs[GPIOMUX_NGPIOS] = {
+	[33] = {
+		.suspended = GSBI1,
+	},
+	[34] = {
+		.suspended = GSBI1,
+	},
+	[35] = {
+		.suspended = GSBI1,
+	},
+	[36] = {
+		.suspended = GSBI1,
+	},
+	[40] = {
+		.suspended = EBI2_CS2,
+	},
+	[43] = {
+		.suspended = GSBI3,
+	},
+	[44] = {
+		.suspended = GSBI3,
+	},
+	[47] = {
+		.suspended = GSBI4,
+	},
+	[48] = {
+		.suspended = GSBI4,
+	},
+	[59] = {
+		.suspended = GSBI7,
+	},
+	[60] = {
+		.suspended = GSBI7,
+	},
+	[64] = {
+		.suspended = GSBI8,
+	},
+	[65] = {
+		.suspended = GSBI8,
+	},
+	[92] = {
+		.suspended = PS_HOLD,
+	},
+	[115] = {
+		.suspended = CONSOLE_UART,
+	},
+	[116] = {
+		.suspended = CONSOLE_UART,
+	},
+	[117] = {
+		.suspended = CONSOLE_UART,
+	},
+	[118] = {
+		.suspended = CONSOLE_UART,
+	},
+	[123] = {
+		.suspended = EBI2_A_D,
+	},
+	[124] = {
+		.suspended = EBI2_A_D,
+	},
+	[125] = {
+		.suspended = EBI2_A_D,
+	},
+	[126] = {
+		.suspended = EBI2_A_D,
+	},
+	[127] = {
+		.suspended = EBI2_A_D,
+	},
+	[128] = {
+		.suspended = EBI2_A_D,
+	},
+	[129] = {
+		.suspended = EBI2_A_D,
+	},
+	[130] = {
+		.suspended = EBI2_A_D,
+	},
+	[133] = {
+		.suspended = EBI2_CS3,
+	},
+	[135] = {
+		.suspended = EBI2_A_D,
+	},
+	[136] = {
+		.suspended = EBI2_A_D,
+	},
+	[137] = {
+		.suspended = EBI2_A_D,
+	},
+	[138] = {
+		.suspended = EBI2_A_D,
+	},
+	[139] = {
+		.suspended = EBI2_A_D,
+	},
+	[140] = {
+		.suspended = EBI2_A_D,
+	},
+	[141] = {
+		.suspended = EBI2_A_D,
+	},
+	[142] = {
+		.suspended = EBI2_A_D,
+	},
+	[143] = {
+		.suspended = EBI2_A_D,
+	},
+	[144] = {
+		.suspended = EBI2_A_D,
+	},
+	[145] = {
+		.suspended = EBI2_A_D,
+	},
+	[146] = {
+		.suspended = EBI2_A_D,
+	},
+	[147] = {
+		.suspended = EBI2_A_D,
+	},
+	[148] = {
+		.suspended = EBI2_A_D,
+	},
+	[149] = {
+		.suspended = EBI2_A_D,
+	},
+	[150] = {
+		.suspended = EBI2_A_D,
+	},
+	[151] = {
+		.suspended = EBI2_OE,
+	},
+	[153] = {
+		.suspended = EBI2_ADV,
+	},
+	[157] = {
+		.suspended = EBI2_WE,
+	},
 	/* SDCC1 data[0] */
 	[159] = {
 		.active = SDCC1_DAT_0_3_CMD_ACTV_CFG,
