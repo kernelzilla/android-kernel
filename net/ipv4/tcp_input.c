73cabd286d5963d21278f982e4de7ed7a68bf05b
Tom Marshall <tdm.code@gmail.com>
2010-09-20 15:42:05 -0700

    tcp: Fix race in tcp_poll
    [ Upstream commit a4d258036ed9b2a1811c3670c6099203a0f284a0 ]
    
    If a RST comes in immediately after checking sk->sk_err, tcp_poll will
    return POLLIN but not POLLOUT.  Fix this by checking sk->sk_err at the end
    of tcp_poll.  Additionally, ensure the correct order of operations on SMP
    machines with memory barriers.
    
    Signed-off-by: Tom Marshall <tdm.code@gmail.com>
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -3962,6 +3962,8 @@ static void tcp_reset(struct sock *sk)
 	default:
 		sk->sk_err = ECONNRESET;
 	}
+	/* This barrier is coupled with smp_rmb() in tcp_poll() */
+	smp_wmb();
 
 	if (!sock_flag(sk, SOCK_DEAD))
 		sk->sk_error_report(sk);
