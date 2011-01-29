32eb740ea1d729de7260b35ea7de613a4ae3dd91
Charles Manning <cdhmanning@gmail.com>
2011-01-29 00:18:46 -0700

    yaffs Fix incorrect handling of deletion flag
    Move the reading of the deleted flag so that it picks up any changes.
    
    Notw we need a value on the stack because the object might get
    freed and we don't want to access a hanging pointer.
    
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>

--- fs/yaffs2/yaffs_guts.c
+++ fs/yaffs2/yaffs_guts.c
@@ -5006,7 +5006,7 @@ static int yaffs_UnlinkFileIfNeeded(yaffs_Object *in)
 int yaffs_DeleteFile(yaffs_Object *in)
 {
 	int retVal = YAFFS_OK;
-	int deleted = in->deleted;
+	int deleted;
 
 	yaffs_ResizeFile(in, 0);
 
@@ -5016,6 +5016,8 @@ int yaffs_DeleteFile(yaffs_Object *in)
 		 */
 		if (!in->unlinked)
 			retVal = yaffs_UnlinkFileIfNeeded(in);
+
+		deleted = in->deleted;
 
 		if (retVal == YAFFS_OK && in->unlinked && !in->deleted) {
 			in->deleted = 1;
