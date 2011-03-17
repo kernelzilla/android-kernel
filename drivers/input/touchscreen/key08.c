a05ca446ab1abf6236975205f66a2f7bf6e4ef72
Patrick Jacques <kernelzilla@kinetic-computing.com>
2011-03-17 14:27:42 -0700

    key08: remove calibration bypass

--- drivers/input/touchscreen/key08.c
+++ drivers/input/touchscreen/key08.c
@@ -1036,10 +1036,9 @@ static void key08_ts_work_func(struct work_struct *work)
 						}
 					}
 
-#ifndef GOOGLE_MULTITOUCH
 					if ( needCalibration )
 						key08_calibrate(ts);
-#endif						
+
 					needCalibration = FALSE;						
 						base = 2;
 #ifdef CONFIG_MACH_MOT
